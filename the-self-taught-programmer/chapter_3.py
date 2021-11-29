03714223c03de3e85d5b849e2d98f57aedff1177
冉申 <ranshen0519@icloud.com>
2021-11-29 20:22:50 +0800

    Create chapter_3.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ the-self-taught-programmer/chapter_3.py
@@ -0,0 +1,41 @@
+# challenge1
+print("Above")
+print("and")
+print("beyond")
+
+
+# challenge2
+x = 11
+if x < 10:
+    print("x is less than 10.")
+else:
+    print("x is greater than or equal to 10")
+
+
+# challenge3
+x = 2
+
+if x <= 10:
+    print("x is less than or equal to 10.")
+elif x <= 25:
+    print("x is greater than 10, but less than or equal to 25.")
+else:
+    print("x is greater than 25.")
+
+
+# challenge4
+print(100 % 13)
+
+
+# challenge5
+print(100 // 3)
+
+
+# challenge6
+age = 64
+retirement = age - 65
+
+if retirement < 10:
+    print("You get to retire soon.")
+else:
+    print("You have a long time until you can retire!")
