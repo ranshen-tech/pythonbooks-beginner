9139bff39b32d95539da12d7b7d9f501e43905d6
冉申 <ranshen0519@icloud.com>
2021-11-30 00:43:11 +0800

    Create chapter_5.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ the-self-taught-programmer/chapter_5.py
@@ -0,0 +1,37 @@
+# challenge1
+fav_musicans = ["Jay Z", "Adventure Club", "John Lennon"]
+
+
+# challenge2
+locations = [(40.7128, 74.0059), (31.0461, 34.8516), (8.3405, 115.0920)]
+
+
+# challenge3
+me = {
+    "height": "6",
+    "fav_color": "red",
+    "fav_author": "Orwell"
+}
+
+
+# challenge4
+# answer = input("Type height, fav_color or fav_author ")
+# if answer in me:
+    # print(me[answer])
+
+
+# challenge5
+songs = {"John Lennon": "Stand by Me",
+         "Kanye West": "Homecoming",
+         "Swedish House Mafia": "Don't You Worry Child"
+}
+
+
+# challenge6
+# 集合（set）是一个无序的不重复元素序列。
+# 可以使用大括号 { } 或者 set() 函数创建集合
+a = set("42341324324")
+print(a)
+print(set("42341324324"))
+b = {"我爱你", "我爱你"}
+print(b)
