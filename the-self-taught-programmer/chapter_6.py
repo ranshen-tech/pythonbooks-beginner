9908fbe1c4bca6bcc4ac144c62ab9e3a040d9188
冉申 <ranshen0519@icloud.com>
2021-11-30 20:15:18 +0800

    Create chapter_6.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ the-self-taught-programmer/chapter_6.py
@@ -0,0 +1,42 @@
+# challenge1
+author = "Camus"
+print(author[0])
+print(author[1])
+print(author[2])
+print(author[3])
+print(author[4])
+
+
+# challenge2
+# answer1 = input("What did you write yesterday? ")
+# answer2 = input("Who did you send it yesterday? ")
+# new_string = "Yesterday I wrote a {}, I send it to {}!".format(answer1, answer2)
+# print(new_string)
+
+
+# challenge3
+print("aldous Huxley was born in 1894.".capitalize())
+
+
+# challenge4
+print("Where now? Who now? When now? ".split(' '))
+
+
+# challenge5
+lst = ["The", "fox", "jumped", "over", "the", "fence", "."]
+print(" ".join(lst))
+
+
+# challenge6
+
+
+
+# challenge7
+
+
+
+# challenge8
+
+
+
+# challenge9
