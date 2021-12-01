112ccf166f65d6cc7a9b4fc59651c7269ac639ed
冉申 <ranshen0519@icloud.com>
2021-12-01 20:08:58 +0800

    Create chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ the-self-taught-programmer/chapter_8/chapter_8.py
@@ -0,0 +1,18 @@
+# challenge1
+import statistics
+
+data = [14, 3, 11, 133, 4]
+
+print(statistics.median_low(data))
+print(statistics.median_high(data))
+print(statistics.median(data))
+
+print(statistics.mean(data))
+# 众数mode：一组数中出现次数最高的
+print(statistics.mode(data))
+
+
+# challenge2
+import cubed
+
+print(cubed.cube_it(3))
