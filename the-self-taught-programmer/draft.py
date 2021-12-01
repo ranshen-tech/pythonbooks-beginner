4bc5e51e2c07d597dd67c890b701bc3091c21144
冉申 <ranshen0519@icloud.com>
2021-12-01 19:15:50 +0800

    Update draft.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- the-self-taught-programmer/draft.py
+++ the-self-taught-programmer/draft.py
@@ -1,11 +1,26 @@
-questions = ["What's your name? ",
-             "What's your favourite color? ",
-             "What's your quest? "]
+# questions = ["What's your name? ",
+#              "What's your favourite color? ",
+#              "What's your quest? "]
 
-n = 0
-while True:
-    print("type 'q' to quit.")
-    answer = input(questions[n])
-    if answer == 'q':
-        break
-    n = (n + 1) % 3
+# n = 0
+# while True:
+#     print("type 'q' to quit.")
+#     answer = input(questions[n])
+#     if answer == 'q':
+#         break
+#     n = (n + 1) % 3
+
+
+# list = [1, 2, 3, 4, 5]
+
+# while True:
+#     print("Type 'q' to quit.")
+#     answer = int(input('猜数字 '))
+#     if answer == 'q':
+#         break
+#     else:
+#         if answer in list:
+#             print('right')
+#         else:
+#             print('wrong')
+
