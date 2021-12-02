e02d93062607b406cd37c6c16985e2e5f45a57f9
冉申 <ranshen0519@icloud.com>
2021-12-02 19:05:22 +0800

    Update chapter_6.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_6.py
+++ python-crash-course/chapter_6.py
@@ -212,4 +212,15 @@ for name, places in favourite_places.items():
         print(f"- {place.title()}")
 
 
-        
+# 6-10
+favourite_numbers = {
+    'mandy': [42, 17],
+    'micah': [42, 39, 56],
+    'gus': [7, 12],
+    }
+
+for name, numbers in favourite_numbers.items():
+    print(f"\n{name.title()} likes the following numbers:")
+    for number in numbers:
+        print(f"- {number}")
+print()
