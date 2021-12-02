0d40ceb26f9079cdd5d502dce6b8fbdd6fd23fd1
冉申 <ranshen0519@icloud.com>
2021-12-02 13:12:43 +0800

    Update chapter_6.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_6.py
+++ python-crash-course/chapter_6.py
@@ -63,6 +63,41 @@ print(f"\n{word.title()}:\n\t{glossary[word]}")
 word = 'dictionary'
 print(f"\n{word.title()}:\n\t{glossary[word]}")
 
-languages = ['python', 'c', 'c', 'ruby']
 
-print(set(languages))
+# 6-4
+glossary = {
+    "string": "A series of characters.",
+    "comment": "A note in a program that the Python interpreter ignores.",
+    "list": "A collection of items in a particular order.",
+    "loop": "Work through a collection of items, one at a time.",
+    "dictionary": "A collection of key-value pairs.",
+    "key": "The first item in a key-value pair in a dictionary.",
+    "value": "An item associated with a key in a dictionary.",
+    "conditional test": "A comparison between two values.",
+    "float": "A numerical value with a decimal component.",
+    "boolean expression": "An expression that evaluates to True or False.",
+    }
+
+for key, value in glossary.items():
+    print(f"\n{key.title()}:\n\t{value}")
+print()
+
+# 6-5
+rivers = {
+    'nile': 'egypt',
+    'mississippi': 'united states',
+    'fraser': 'canada',
+    'kuskokwim': 'alaska',
+    'yangtze': 'china',
+}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print(f"- {river.title()}")
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print(f"- {country.title()}")
