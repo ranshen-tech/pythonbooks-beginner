35bf0088bd5c04141387d2aea033f27eddbcd9ec
冉申 <ranshen0519@icloud.com>
2021-12-02 23:02:30 +0800

    Update chapter_7.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_7.py
+++ python-crash-course/chapter_7.py
@@ -1,17 +1,39 @@
-# 7-1
-car = input("What kind of car would you like? ")
+# # 7-1
+# car = input("What kind of car would you like? ")
 
-print(f"Let me see if I can find you a {car.title()}.")
-
-
-# 7-2
-party_size = input("How many people are in your dinner party tonight? ")
-party_size = int(party_size)
-
-if party_size > 8:
-    print("I'm sorry, you'll have to wait for a table.")
-else:
-    print("Your table is ready.")
+# print(f"Let me see if I can find you a {car.title()}.")
 
 
-# 7-3
+# # 7-2
+# party_size = input("How many people are in your dinner party tonight? ")
+# party_size = int(party_size)
+
+# if party_size > 8:
+#     print("I'm sorry, you'll have to wait for a table.")
+# else:
+#     print("Your table is ready.")
+
+
+# # 7-3
+# number = input("Give me a number, please: ")
+# number = int(number)
+
+# if number % 10 == 0:
+#     print(f"{number} is a multiple of 10.")
+# else:
+#     print(f"{number} is not a multiple of 10.")
+
+
+# 7-4
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    topping = input(prompt)
+    if topping != 'quit':
+        print(f"- I'll add {topping} to your pizza.")
+    else:
+        break
+
+
+# 7-5
