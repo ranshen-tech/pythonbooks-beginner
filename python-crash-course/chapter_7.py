418f4aa5e23f38b5262f2470246123cde3f284b5
冉申 <ranshen0519@icloud.com>
2021-12-02 21:56:21 +0800

    Create chapter_7.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ python-crash-course/chapter_7.py
@@ -0,0 +1,17 @@
+# 7-1
+car = input("What kind of car would you like? ")
+
+print(f"Let me see if I can find you a {car.title()}.")
+
+
+# 7-2
+party_size = input("How many people are in your dinner party tonight? ")
+party_size = int(party_size)
+
+if party_size > 8:
+    print("I'm sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready.")
+
+
+# 7-3
