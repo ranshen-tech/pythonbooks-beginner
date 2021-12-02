8cb8caee7b41a0f47b5cf2538ec7373e0dc70970
冉申 <ranshen0519@icloud.com>
2021-12-02 18:44:56 +0800

    Update chapter_6.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_6.py
+++ python-crash-course/chapter_6.py
@@ -164,3 +164,37 @@ print()
 
 
 # 6-8
+pets = []
+
+pet = {
+    'animal type': 'python',
+    'name': 'john',
+    'owner': 'guido',
+    'weight': 43,
+    'eats': 'bugs',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'chicken',
+    'name': 'clarence',
+    'owner': 'tiffany',
+    'weight': 2,
+    'eats': 'seeds',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'dog',
+    'name': 'peso',
+    'owner': 'eric',
+    'weight': 37,
+    'eats': 'shoes',
+}
+pets.append(pet)
+
+for pet in pets:
+    print(f"Here's what I know about {pet['name'].title()}:")
+    for key, value in pet.items():
+        print(f"\t{key}: {value}")
+print()
