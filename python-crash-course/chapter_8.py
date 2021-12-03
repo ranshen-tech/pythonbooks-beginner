ac4e061e46647c21c652f50b28b3cb86cc230f73
冉申 <ranshen0519@icloud.com>
2021-12-03 21:27:49 +0800

    Update chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -79,7 +79,7 @@ album = make_album('beethoven', 'ninth symphony')
 print(album)
 album = make_album('willie nelson', 'red-headed stranger')
 print(album)
-
+print()
 
 # 8-7-2
 def make_album(artist, title, tracks=0):
@@ -88,8 +88,8 @@ def make_album(artist, title, tracks=0):
         'artist': artist.title(),
         'title': title.title(),
         }
-        if tracks:
-            album_dict['tracks'] = tracks
+    if tracks:
+        album_dict['tracks'] = tracks
     return album_dict
 
 album = make_album('metalllica', 'ride the lightning')
@@ -100,3 +100,30 @@ album = make_album('willie nelson', 'red-headed stranger')
 print(album)
 album = make_album('iron maiden', 'piece of mind', tracks=8)
 print(album)
+
+
+# 8-8
+def make_album(artist, title, tracks=0):
+    """创建一个包含专辑信息的字典。"""
+    album_dict = {
+       'artist': artist.title(),
+       'title': title.title(),
+       }
+    if tracks:
+       album_dict['tracks'] = tracks
+    return album_dict
+# 生成提示语。
+title_prompt = "\nWhat album are you thinking of? "
+artist_prompt = "Who's the artist? "
+# 让用户知道如何退出。
+print("Enter 'quit' at any time to stop.")
+while True:
+    title = input(title_prompt)
+    if title == 'quit':
+        break
+    artist = input(artist_prompt)
+    if artist == 'quit':
+       break
+    album = make_album(artist, title)
+    print(album)
+print("\nThanks for responding!")
