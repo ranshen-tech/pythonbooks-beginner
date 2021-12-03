303c4b27dc4719ba02caee636937529ff70565fd
冉申 <ranshen0519@icloud.com>
2021-12-03 21:00:56 +0800

    Update chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -65,3 +65,17 @@ print()
 
 
 # 8-7
+def make_album(artist, title):
+    """创建一个包含专辑信息的字典"""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+        }
+    return album_dict
+
+album = make_album('metallica', 'ride the lightning')
+print(album)
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
