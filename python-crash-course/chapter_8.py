0929439cb9fb76940ad3142f7a2b3dff22969d26
冉申 <ranshen0519@icloud.com>
2021-12-04 00:11:22 +0800

    Update chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -175,13 +175,16 @@ def show_messages(messages):
     for message in messages:
        print(message)
 def send_messages(messages, sent_messages):
-    """打印每条消息，再将其移到列表 sent_messages 中。""" print("\nSending all messages:")
-while messages:
+    """打印每条消息，再将其移到列表 sent_messages 中。"""
+    print("\nSending all messages:")
+    while messages:
        current_message = messages.pop()
        print(current_message)
        sent_messages.append(current_message)
+
 messages = ["hello there", "how are u?", ":)"]
 show_messages(messages)
+
 sent_messages = []
 send_messages(messages[:], sent_messages)
 print("\nFinal lists:")
