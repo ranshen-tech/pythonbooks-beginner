96efa23fc042d2819f0e2663dab79d82221184e5
冉申 <ranshen0519@icloud.com>
2021-12-03 23:51:19 +0800

    Update chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -127,3 +127,14 @@ while True:
     album = make_album(artist, title)
     print(album)
 print("\nThanks for responding!")
+print()
+
+
+# 8-9
+def show_messages(messages):
+    """打印列表中的所有消息"""
+    for message in messages:
+        print(message)
+
+messages = ['hello there', 'how are u?', ':)']
+show_messages(messages)
