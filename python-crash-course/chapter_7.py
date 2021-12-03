5e87bcdc29aa2dc5e5b7e8e6bcb34bbcb62347a2
冉申 <ranshen0519@icloud.com>
2021-12-03 12:18:27 +0800

    Update chapter_7.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_7.py
+++ python-crash-course/chapter_7.py
@@ -37,17 +37,52 @@
 
 
 # 7-5
-prompt = "How old are you?"
-prompt += "\nEnter 'quit' when you are finished. "
+# prompt = "How old are you?"
+# prompt += "\nEnter 'quit' when you are finished. "
 
-while True:
-    age = input(prompt)
-    if age == 'quit':
-        break
-    age = int(age)
-    if age < 3:
-        print("You get in free!")
-    elif age < 13:
-        print("Your ticket is $10.")
-    else:
-        print("Your ticket is $15.")
+# while True:
+#     age = input(prompt)
+#     if age == 'quit':
+#         break
+#     age = int(age)
+#     if age < 3:
+#         print("You get in free!")
+#     elif age < 13:
+#         print("Your ticket is $10.")
+#     else:
+#         print("Your ticket is $15.")
+
+
+# 7-8
+sandwich_orders =['veggie', 'grilled cheese', 'turkey', 'roast beef']
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I'm working on your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print()
+for sandwich in finished_sandwiches:
+    print(f"I made a {sandwich} sandwich.")
+print('\n')
+
+
+# 7-9
+sandwich_orders = [
+    'pastrami', 'veggie', 'grilled cheese', 'pastrami',
+    'turkey', 'roast beef', 'pastrami'
+    ]
+finished_sandwiches = []
+
+print("I'm sorry, we're all out of pastrami today.")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+print()
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I'm working on your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+print()
+for sandwich in finished_sandwiches:
+    print(f"I made a {sandwich} sandwich.")
