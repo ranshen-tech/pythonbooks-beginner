7480217c5bf5d9e315bf04ea014c991f1daee703
冉申 <ranshen0519@icloud.com>
2021-12-04 23:38:18 +0800

    Update chapter_8.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -242,16 +242,18 @@ print(my_outback)
 my_old_accord = make_car('honda', 'accord', year=1991, color='white',
        headlights='popup')
 print(my_old_accord)
+print('\n')
 
 
 def make_car(manufacturer, model, **options):
     """创建一个表示汽车的字典。"""
-    options['manufacturer']: manufacturer.title()
-    options['model']: model.title()
+    options['manufacturer']: manufacturer
+    options['model']: model
     for option, value in options.items():
         options[option] = value
     
     return options
+
 my_outback = make_car('subaru', 'outback', color='blue', tow_package=True)
 print(my_outback)
  
