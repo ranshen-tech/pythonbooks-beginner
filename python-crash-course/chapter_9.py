a1186e20caa7613bb04538e49bdb7c3847791c88
冉申 <ranshen0519@icloud.com>
2021-12-05 23:51:39 +0800

    Update chapter_9.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_9.py
+++ python-crash-course/chapter_9.py
@@ -241,9 +241,10 @@ class User():
 
     def describe_user(self):
         """显示用户信息摘要。"""
-        print(f"\n{self.first_name} {self.last_name}") print(f" Username: {self.username}")
-print(f" Email: {self.email}")
-print(f" Location: {self.location}")
+        print(f"\n{self.first_name} {self.last_name}")
+        print(f" Username: {self.username}")
+        print(f" Email: {self.email}")
+        print(f" Location: {self.location}")
 def greet_user(self): """向用户发出个性化问候。""" print(f"\nWelcome back, {self.username}!")
 def increment_login_attempts(self): """将属性 login_attempts 的值加 1。""" self.login_attempts += 1
 def reset_login_attempts(self): """将 login_attempts 重置为 0。""" self.login_attempts = 0
