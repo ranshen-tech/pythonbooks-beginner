25f0e840b6c9ea18fcf3b2a9749431c5d30450b1
冉申 <ranshen0519@icloud.com>
2021-12-07 00:20:05 +0800

    Update chapter_9.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_9.py
+++ python-crash-course/chapter_9.py
@@ -359,4 +359,64 @@ eric.privileges.privileges = eric_privileges
 eric.privileges.show_privileges()
 
 # 9-9
+class Car(): """一次模拟汽车的简单尝试。"""
+def __init__(self, manufacturer, model, year): """初始化描述汽车的属性。""" self.manufacturer = manufacturer self.model = model
+·73·
+练习答案
+         self.year = year
+       self.odometer_reading = 0
+def get_descriptive_name(self):
+"""返回整洁的描述性信息。"""
+long_name = f"{self.year} {self.manufacturer} {self.model}" return long_name.title()
+def read_odometer(self):
+"""打印一条指出汽车里程的消息。"""
+print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self, mileage):
+       """
+将里程表读数设置为指定的值。 禁止将里程表读数往回调。
+"""
+if mileage >= self.odometer_reading:
+           self.odometer_reading = mileage
+       else:
+           print("You can't roll back an odometer!")
+def increment_odometer(self, miles): """将里程表读数增加指定的量。""" self.odometer_reading += miles
+class Battery(): """一次模拟电动汽车电瓶的简单尝试。"""
+def __init__(self, battery_size=75): """初始化电瓶的属性。""" self.battery_size = battery_size
+def describe_battery(self):
+"""打印一条描述电瓶容量的消息。"""
+print(f"This car has a {self.battery_size}-kWh battery.")
+·74·
 
+练习答案
+  def get_range(self): """打印一条消息，指出电瓶的续航里程。""" if self.battery_size == 75:
+           range = 260
+       elif self.battery_size == 100:
+range = 315
+       message = f"This car can go approximately {range}"
+       message += " miles on a full charge."
+       print(message)
+def upgrade_battery(self): """在可能的情况下将电瓶升级。""" if self.battery_size == 75:
+           self.battery_size = 100
+           print("Upgraded the battery to 100 kWh.")
+       else:
+           print("The battery is already upgraded.")
+class ElectricCar(Car): """电动汽车的独特之处。"""
+    def __init__(self, manufacturer, model, year):
+       """
+初始化父类的属性。
+再初始化电动汽车特有的属性。
+"""
+super().__init__(manufacturer, model, year) self.battery = Battery()
+print("Make an electric car, and check the battery:")
+my_tesla = ElectricCar('tesla', 'roadster', 2019)
+my_tesla.battery.describe_battery()
+print("\nUpgrade the battery, and check it again:")
+my_tesla.battery.upgrade_battery()
+·75·
+
+练习答案
+ 
+my_tesla.battery.describe_battery()
+print("\nTry upgrading the battery a second time.")
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
