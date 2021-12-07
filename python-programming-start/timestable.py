9ff703349bfdb2e48307f2d358969031ad8a905d
冉申 <ranshen0519@icloud.com>
2021-12-07 14:40:29 +0800

    Create timestable.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ python-programming-start/timestable.py
@@ -0,0 +1,12 @@
+for row in range(1, 3):
+    for col in range(1, 3):
+        prod = row * col
+        if prod < 10:
+            print('', end='')
+        print(prod, '', end='')
+
+# print(0, 0, 0, 0)
+# print(1, 1, 1, 1)
+# print('', 2)
+# print('', end='')
+# print(1, 2)
