8d6c8d757be9cffc28cb7f2ca0c4cb8aa7a5c3b3
冉申 <ranshen0519@icloud.com>
2021-12-08 19:02:09 +0800

    Update die.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_9/die.py
+++ python-crash-course/chapter_9/die.py
@@ -5,11 +5,21 @@ class Die():
     """一个表示骰子的类"""
 
     def __init__(self, sides=6):
+        """初始化骰子"""
         self.sides = sides
 
     def roll_die(self):
-        print(randint(1, self.sides))
+        """返回一个位于 1 和骰子面数之间的随机数。"""
+        return randint(1, self.sides)
 
-die_6 = Die()
-for i in range(10):
-    die_6.roll_die()
+# 移到my_die.py文件中
+
+# # 创建一个 6 面的骰子，再掷 10 次并显示结果。
+# d6 = Die()
+
+# results = []
+# for roll_num in range(10):
+#     result = d6.roll_die()
+#     results.append(result)
+# print("\n10 rolls of a 6-sided die:")
+# print(results)
