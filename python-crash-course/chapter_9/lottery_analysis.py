dca8246db20361f4e99772b8813756dcd3d90703
冉申 <ranshen0519@icloud.com>
2021-12-09 00:22:23 +0800

    Update lottery_analysis.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_9/lottery_analysis.py
+++ python-crash-course/chapter_9/lottery_analysis.py
@@ -4,10 +4,8 @@ from random import choice
 def get_winning_ticket(possibilities):
     """摇出中奖组合。"""
     winning_ticket = []
-    # 中奖组合中不能包含重复的数字或字母，因此使用了 while 循环.
     while len(winning_ticket) < 4:
         pulled_item = choice(possibilities)
-        # 仅当摇出的数字或字母不在组合中时，才将其添加到组合中.
         if pulled_item not in winning_ticket:
             winning_ticket.append(pulled_item)
     return winning_ticket
@@ -17,17 +15,13 @@ def check_ticket(played_ticket, winning_ticket):
     for element in played_ticket:
        if element not in winning_ticket:
            return False
-    # 如果代码执行到这里，就说明中奖了!
     return True
 
 def make_random_ticket(possibilities):
     """随机地生成彩票。"""
     ticket = []
-
-    # 彩票不能包含重复的数字或字母，因此使用了 while 循环。
     while len(ticket) < 4:
         pulled_item = choice(possibilities)
-        # 仅当随机生成的数字或字母不在彩票中时，才将其添加到彩票中。
         if pulled_item not in ticket:
             ticket.append(pulled_item)
     return ticket
@@ -36,8 +30,8 @@ possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
 winning_ticket = get_winning_ticket(possibilities)
 plays = 0
 won = False
-# 为避免程序执行时间太长，设置最多随机生成多少张彩票。 
 max_tries = 1_000_000
+
 while not won:
     new_ticket = make_random_ticket(possibilities)
     won = check_ticket(new_ticket, winning_ticket)
