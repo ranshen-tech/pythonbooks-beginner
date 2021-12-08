7031c0198e358d32e5fbac6f833e2f42f964076a
冉申 <ranshen0519@icloud.com>
2021-12-08 21:18:20 +0800

    Update lottery.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_9/lottery.py
+++ python-crash-course/chapter_9/lottery.py
@@ -3,7 +3,7 @@ from random import choice
 possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
 
 winning_ticket = []
-print("Let's see what the winning ticket is...")
+print("\nLet's see what the winning ticket is...")
 
 # 中奖组合中不能包含重复的数或字母，因此使用了 while 循环。
 while len(winning_ticket) < 4:
@@ -12,3 +12,4 @@ while len(winning_ticket) < 4:
     if pulled_item not in winning_ticket:
         print(f"We pulled a {pulled_item}!")
         winning_ticket.append(pulled_item)
+print(f"\nWe final winning ticket is {winning_ticket}")
