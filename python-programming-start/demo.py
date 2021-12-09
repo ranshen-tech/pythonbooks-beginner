563a064bd357561c2762a6a8665366501fdd1149
冉申 <ranshen0519@icloud.com>
2021-12-09 20:01:35 +0800

    Update demo.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-programming-start/demo.py
+++ python-programming-start/demo.py
@@ -1,32 +1,9 @@
-# import math
+CHAR = '*'
+def rectangle(height, width):
+    """prints the rectangle."""
+    for row in range(height):
+        for col in range(width):
+            print(CHAR, end='')
+        print()
 
-# def area(radius):
-#     """returns the area of a circle with the given radius.
-#     for example:
-#         >>> print(area(1))
-#         3.141592653589793
-#     """
-#     return math.pi * radius ** 2
-
-# print(area(1))
-# print(area(5.5))
-# print(2 * ((area(3) + area(4))))
-# print(area.__doc__)
-
-# def main():
-#     password = input("What's your password? ")
-#     if password == 'apple':
-#         print("Logging on...")
-#     else:
-#         print("Incorrect password.")
-#     print("All Done.")
-
-# main()
-
-def set1(x):
-    x = 1
-    return x
-
-m = 5
-print(set1(m))
-print(set1(5))
+rectangle(3, 5)
