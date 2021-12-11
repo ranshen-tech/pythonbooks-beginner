87cd348f5238355ccb9dd65376da60384b2aba4e
冉申 <ranshen0519@icloud.com>
2021-12-11 14:43:16 +0800

    Create shapes.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ python-programming-start/shapes.py
@@ -0,0 +1,24 @@
+CHAR = '*'
+def rectangle(height, width):
+    """prints the rectangle."""
+    for row in range(height):
+        for col in range(width):
+            print(CHAR, end='')
+        print()
+
+def square(side):
+    """prints a square."""
+    rectangle(side, side)
+
+def triangle(height):
+    """prints a right triangle."""
+    for row in range(height):
+        for col in range(1, row+2):
+            print(CHAR, end='')
+        print()
+
+rectangle(2, 2)
+print()
+square(2)
+print()
+triangle(2)
