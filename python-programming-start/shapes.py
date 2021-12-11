82437a2b39ddc4150bd6136e191535e3164650f1
冉申 <ranshen0519@icloud.com>
2021-12-11 19:52:33 +0800

    Update shapes.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-programming-start/shapes.py
+++ python-programming-start/shapes.py
@@ -1,10 +1,1 @@
-def codesum(s):
-    """Returns the sums of the character codes of s."""
-    total = 0
-    for c in s:
-        total = total + ord(c)
-    return total
-
-print(codesum('ranshen'))
-
-print(ord.__doc__)
+print(len('\t'))
