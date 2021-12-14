333bc0d2557283aa520abdf8c9e90ef160efad5c
冉申 <ranshen0519@icloud.com>
2021-12-14 10:33:10 +0800

    Update character-count.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- automate-the-boring-stuff/character-count.py
+++ automate-the-boring-stuff/character-count.py
@@ -1,9 +1,9 @@
-message = 'It was a bright cold day in April,'
-message += ' and the clocks were striking thirteen.'
+message = 'It was a bright could day in April,'
+message += 'and the clocks were striking thirteen.'
 
 count = {}
 for character in message:
     count.setdefault(character, 0)
-    # count[character] = count[character] + 1
+    count[character] += 1
 
 print(count)
