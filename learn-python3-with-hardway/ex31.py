a8a1125c025f9ab8152b8b2e81f038cfdbc19883
冉申 <ranshen0519@icloud.com>
2021-12-27 10:55:59 +0800

    Create ex31.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learn-python3-with-hardway/ex31.py
@@ -0,0 +1,36 @@
+door = input("""
+You enter a dark room with two doors.
+Do you go through door #1 or door #2?
+> """)
+
+if door == '1':
+    bear = input("""There's a giant bear here eating a cheese cake.
+What do you do?
+1. Take the cake.")
+2. Scream at the bear.
+> """)
+
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == '2':
+        print("The bear eats your legs off. Good job!")
+    else:
+        print(f"Well, doing {bear} is probably better.")
+        print("Bear runs away.")
+
+elif door == "2":
+    insanity = input("""You stare into the endless abyss at Cthulhu's retina.
+1. Blueberries.
+2. Yellow jacket clothespins.
+3. Understanding revolvers yelling melodies.
+> """)
+
+    if insanity == '1' or insanity == '2':
+        print("Your body survives powered by a mind of jello.")
+        print("Good job!")
+    else:
+        print("The insanity rots your eyes into a pool of muck.")
+        print("Good job!")
+
+else:
+    print("You stumble around and fall on a knife and die. Good job!")
