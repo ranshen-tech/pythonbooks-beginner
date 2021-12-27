5ec7f27ab854fa778069f45f34a672640c885342
冉申 <ranshen0519@icloud.com>
2021-12-27 23:36:45 +0800

    Update ex36.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learn-python3-with-hardway/ex36.py
+++ learn-python3-with-hardway/ex36.py
@@ -4,3 +4,23 @@ from sys import exit
 def dead(why):
     print(why, 'Good job!')
     exit(0)
+
+def start():
+    print("""
+    A forest is in front of you.
+    What are you going to do ?
+    """)
+    
+    choice = input(">>> ")
+    a = choice.find('straight')
+    b = choice.find('back')
+
+    if a != -1:
+        dead('You dead in the forest due to the fear.')
+    elif b != -1:
+        print("Oh yeah! You're smart, fools don't look back.")
+        exit(0)
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
