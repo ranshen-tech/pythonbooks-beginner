98057d4b868c4f4f7073a9f21f49cb40e5b662ac
冉申 <ranshen0519@icloud.com>
2021-12-28 23:53:17 +0800

    Update ex41.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learn-python3-with-hardway/ex41.py
+++ learn-python3-with-hardway/ex41.py
@@ -1,5 +1,37 @@
- import random
- from urllib.request import urlopen
- import sys
+import random
+from urllib.request import urlopen
+import sys
 
- WORL_URL = "http://learncodethehardway.org/word.txt"
+WORL_URL = "http://learncodethehardway.org/word.txt"
+WORL_URL = []
+
+PHRASES = {
+    "class %%%(%%%):"
+    "Make a class named %%% that is-a %%%",
+    'class %%%(object):/n/tdef __init__(self, ***)':
+    'class %%% has-a __init__ that takes self and *** params.',
+    'class %%%(object):/n/tdef ***(self, @@@)':
+    'class %%% has-a function *** that takes self and @@@params.'
+    '*** = %%%()':
+    'Set *** to an instance of class %%%.',
+    '***.***(@@@)':
+    "From *** get the *** function, call it with params self, @@@.",
+    "***.*** = '***'":
+    "From *** get the *** attribute and set it to '***'."
+}
+
+# do they want to drill phrases first
+if len(sys.argv) == 2 and sys.argv[1] == 'english':
+    PHRASE_FIRST =True
+else:
+    PHRASE_FIRST = False
+
+# load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(str(word.strip(), encoding='utf-8'))
+
+
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count('%%%'))]
+
+    
