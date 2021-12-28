2697c1d855d60740eb36bd0719beb8ad44926b2a
冉申 <ranshen0519@icloud.com>
2021-12-28 22:13:22 +0800

    Create ex40a.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learn-python3-with-hardway/ex40a.py
@@ -0,0 +1,25 @@
+class MyStuff(object):
+    
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I am classy apples!")
+
+# 实例化操作
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+# dict style
+# mystuff['apples']
+
+# module style
+# mystuff.apples()
+# print(mystuff.tangerine)
+
+# class style
+# thing = MyStuff()
+# thing.apples()
+# print(thing.tangerine)
