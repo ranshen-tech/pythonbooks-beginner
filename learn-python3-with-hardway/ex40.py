153d0d52175f4a36e008df3b9f9fa67315c9aedf
冉申 <ranshen0519@icloud.com>
2021-12-28 22:37:39 +0800

    Update ex40.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learn-python3-with-hardway/ex40.py
+++ learn-python3-with-hardway/ex40.py
@@ -7,9 +7,13 @@ class Song(object):
         for line in self.lyrics:
             print(line)
 
-happy_bday = Song(["Happy birthday to you",
-                   "I don't want to get sued",
-                   "So I'll stop right there"])
+songs1 = [
+    "Happy birthday to you",
+    "I don't want to get sued",
+    "So I'll stop right there"
+    ]
+
+happy_bday = Song(songs1)
 
 bulls_on_parade = Song(["They rally around the family",
                         "With pockets full of shells"])
