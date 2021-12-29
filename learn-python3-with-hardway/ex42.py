208d62c02d85fdaaf23bc5ba33b8b1afa5e5b133
冉申 <ranshen0519@icloud.com>
2021-12-29 23:51:47 +0800

    Update ex42.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learn-python3-with-hardway/ex42.py
+++ learn-python3-with-hardway/ex42.py
@@ -1,23 +1,20 @@
-# def pivotIndex(nums):
-#     a = 0
-#     for i in range(len(nums)):
-#         b = sum(nums) - a - nums[i]
-#         if a == b:
-#             return i
-#         a += nums[i]
-#     return -1
+## Animalis-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
 
-# nums = [1,2,3,4,5]
-# print(pivotIndex(nums))
+## ??
+class Dog(Animal):
+    
+    def __init__(self, name):
+        ## ??
+        self.name = name
 
+## ??
+class Cat(Animal):
 
-def pivotIndex(nums):
-    a = 0
-    for i in range(len(nums)):
-        if a * 2 + nums[i]== sum(nums):
-            return i
-        a += nums[i]
-    return -1
+    def __init__(self, name):
+        ## ??
+        self.name = name
 
-nums = [1,2,3,3]
-print(pivotIndex(nums))
+## ??
+class Person(object):
