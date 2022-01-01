15140bad6640c7c9323c18865867f8f5824b0828
冉申 <ranshen0519@icloud.com>
2022-01-01 15:00:45 +0800

    Update ex43.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learn-python3-with-hardway/ex43.py
+++ learn-python3-with-hardway/ex43.py
@@ -13,6 +13,7 @@ class Engine(object):
     def __init__(self, scene_map):# self.start_scene = 'central_corridor'
         self.scene_map = scene_map
 
+
     def play(self):
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
