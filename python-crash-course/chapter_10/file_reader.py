7fe78855f153579724f2d3800190e440b41f6cd2
冉申 <ranshen0519@icloud.com>
2022-01-01 22:52:43 +0800

    Update file_reader.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/file_reader.py
+++ python-crash-course/chapter_10/file_reader.py
@@ -1,4 +1,6 @@
 pi_digits = 'pythonbooks-beginner/python-crash-course/chapter_10/pi_digits.txt'
 with open(pi_digits) as file_object:
-    contents = file_object.read()
-print(contents)
+    lines = file_object.readlines()
+print(lines)
+for line in lines:
+    print(line.rstrip())
