3693b4ebe01462b907022eb961df3e6ddbca4278
冉申 <ranshen0519@icloud.com>
2022-01-04 17:10:33 +0800

    Update demo.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/demo.py
+++ python-crash-course/chapter_10/demo.py
@@ -12,17 +12,22 @@ def get_stored_username():
         return username
 
 
+def get_new_username():
+    """提示用户输入用户名"""
+    username = input("What is your name?")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+
 def greet_user():
     """问候用户，并指出其名字"""
     username = get_stored_username()
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        filename = 'username.json'
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come back, {username}!")
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
-print(None == False)
