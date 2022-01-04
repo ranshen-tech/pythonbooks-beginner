92566cad04822cb2091f652b2657f08177582385
冉申 <ranshen0519@icloud.com>
2022-01-04 16:38:54 +0800

    Update demo.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/demo.py
+++ python-crash-course/chapter_10/demo.py
@@ -1,17 +1,27 @@
 import json
 
-def greet_user():
-    """问候用户，并指出其名字"""
+def get_stored_username():
+    """如果存储了用户名，就获取它"""
     filename = 'username.json'
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def greet_user():
+    """问候用户，并指出其名字"""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
         username = input("What is your name? ")
+        filename = 'username.json'
         with open(filename, 'w') as f:
             json.dump(username, f)
             print(f"We'll remember you when you come back, {username}!")
-    else:
-        print(f"Welcome back, {username}!")
 
 greet_user()
