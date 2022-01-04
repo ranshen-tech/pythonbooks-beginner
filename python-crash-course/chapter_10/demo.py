e4c7f00018a125dabf39e1305616d8b636be4c35
冉申 <ranshen0519@icloud.com>
2022-01-04 15:44:41 +0800

    Update demo.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/demo.py
+++ python-crash-course/chapter_10/demo.py
@@ -1,1 +1,13 @@
 import json
+# 如果以前存储了用户名，就加载它。否则，提示用户输入用户名并存储它。
+filename = 'username.jsons'
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll rememeber you when you come back, {username}!")
+else:
+    print(f"Welcome back, {username}!")
