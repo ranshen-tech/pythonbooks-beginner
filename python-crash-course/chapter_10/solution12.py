c846ea6057ec65378ccd6e2d40402aad603f7c31
冉申 <ranshen0519@icloud.com>
2022-01-05 16:22:04 +0800

    Update solution12.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/solution12.py
+++ python-crash-course/chapter_10/solution12.py
@@ -3,12 +3,13 @@ import json
 try:
     with open('favourite_number.json') as f:
         favourite_number = json.load(f)
+
 except FileNotFoundError:
-    pass
+    favourite_number = input("What your favourite number? ")
+    with open('favourite_number.json', 'w') as f:
+        json.dump(favourite_number, f)
+    print(f"Thanks, I'll remember that.")
+
 else:
     print(f"I know your favourite number, It's {favourite_number}")
 
-# favourite_number = input("What your favourite number? ")
-# with open('favourite_number.json', 'w') as f:
-#     json.dump(favourite_number, f)
-# print(f"Thanks, I'll remember that.")
