c5039423d3e6c98861f9618455df566226929adf
冉申 <ranshen0519@icloud.com>
2022-01-05 21:45:01 +0800

    Update solution13.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- python-crash-course/chapter_10/solution13.py
+++ python-crash-course/chapter_10/solution13.py
@@ -19,14 +19,19 @@ def get_new_username():
        json.dump(username, f_obj)
     return username
 
-def greet_user():
-    """基于用户名问候用户。"""
+def check_username():
+    """检查用户名是否正确"""
     username = get_stored_username()
     if username:
-       correct = input(f"Are you {username}? (y/n) ")
-       if correct == 'y':
-           print(f"Welcome back, {username}!")
-           return
+        correct = input(f"Are you {username}? (y/n) ")
+        return correct
+
+def greet_user():
+    """基于用户名问候用户。"""
+
+    if check_username() == 'y':
+        print(f"Welcome back, {check_username()}!")
+        return
 
     username = get_new_username()
     print(f"We'll remember you when you come back, {username}!")
