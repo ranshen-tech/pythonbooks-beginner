5f91ca51d7a338e4997362653cef743e2f97fb40
冉申 <ranshen0519@icloud.com>
2022-01-21 17:57:51 +0800

    Update module.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python/chapter-3/module.py
+++ learning-python/chapter-3/module.py
@@ -3,3 +3,13 @@ print(myfile.title)
 
 from myfile import title
 print(title)
+
+import threenames
+print(threenames.a)
+print(threenames.b, threenames.c)
+
+from threenames import a, b, c
+print(a)
+print(b, c)
+
+print(dir(threenames))
