1d4f3ba84bc7b53c9242787bafe41b3371daf2d3
冉申 <ranshen0519@icloud.com>
2022-01-29 21:10:03 +0800

    Update spam.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python/chapter-4/spam.py
+++ learning-python/chapter-4/spam.py
@@ -58,3 +58,12 @@ print(diag)
 
 doubles = [c * 2 for c in 'spam']
 print(doubles)
+
+print(list(range(4)))
+print(list(range(-6, 7, 2)))
+
+print([[x ** 2, x ** 3] for x in range(4)])
+print([[x, x / 2, x * 2] for x in range(-6, 7, 2) if x > 0])
+
+G = [sum(row) for row in M]
+print(G)
