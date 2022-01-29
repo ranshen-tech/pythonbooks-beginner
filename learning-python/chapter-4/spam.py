b1c70554792c51ab9993d21cf50f32dd342d3c91
冉申 <ranshen0519@icloud.com>
2022-01-29 15:30:55 +0800

    Update spam.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python/chapter-4/spam.py
+++ learning-python/chapter-4/spam.py
@@ -1,14 +1,14 @@
-# import random
-# print(random.random())
-# print(random.choice([1, 2, 3, 4]))
+import random
+print(random.random())
+print(random.choice([1, 2, 3, 4]))
 
-# line = 'aaa,bbb,ccccc,dd'
-# print(line.split(','))
+line = 'aaa,bbb,ccccc,dd'
+print(line.split(','))
 
-# S = 'spam'
-# print(S.upper())
-# print(S.isalpha())
-# print(S.isdigit())
+S = 'spam'
+print(S.upper())
+print(S.isalpha())
+print(S.isdigit())
 
 line = 'aaa,bbb,ccccc,dd\n'
 print(line)
