e1030c3f2ba22dad4055e7de3baa44604c831710
冉申 <ranshen0519@icloud.com>
2022-01-29 23:56:23 +0800

    Update spam.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python/chapter-4/spam.py
+++ learning-python/chapter-4/spam.py
@@ -65,5 +65,9 @@ print(list(range(-6, 7, 2)))
 print([[x ** 2, x ** 3] for x in range(4)])
 print([[x, x / 2, x * 2] for x in range(-6, 7, 2) if x > 0])
 
-G = [sum(row) for row in M]
-print(G)
+G = (sum(row) for row in M)
+print(next(G))
+print(next(G))
+print(next(G))
+
+print(list(map(sum, M)))
