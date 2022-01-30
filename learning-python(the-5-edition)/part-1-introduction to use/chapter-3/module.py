5e74f45e167b31f99b35b0535cd681474c076f40
冉申 <ranshen@MacBook-Pro.local>
2022-01-30 19:22:41 +0800

    Create module.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-1-introduction to use/chapter-3/module.py
@@ -0,0 +1,17 @@
+import myfile
+print(myfile.title)
+
+from myfile import title
+print(title)
+
+import threenames
+print(threenames.a)
+print(threenames.b, threenames.c)
+
+from threenames import a, b, c
+print(a)
+print(b, c)
+
+print(dir(threenames))
+
+import module1
