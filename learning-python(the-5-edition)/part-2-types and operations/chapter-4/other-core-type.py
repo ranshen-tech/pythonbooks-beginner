eedd99bd3bf253ac2a8952ea8a277920f584448c
冉申 <ranshen0519@icloud.com>
2022-01-31 23:23:17 +0800

    Update other-core-type.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-4/other-core-type.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-4/other-core-type.py
@@ -33,3 +33,44 @@ from fractions import Fraction
 f = Fraction(2, 3)
 print(f + 1)
 print(f + Fraction(1, 2))
+
+print(1 > 2, 1 < 2)
+print(bool("spam"))
+
+X = None
+print(X)
+L = [None] * 100
+print(L)
+
+
+# 如何破坏代码的灵活性
+print(type(L))
+print(type(type(L)))
+
+if type(L) == type([]):
+    print("yes")
+if type(L) == list:
+    print("yes")
+if isinstance(L, list):
+    print("yes")
+
+
+# 用户定义的类
+class Worker:
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay *= 1.0 + percent
+
+
+bob = Worker("Bob Smith", 50000)
+sue = Worker("Sue Jones", 60000)
+print(bob.lastName())
+print(sue.lastName())
+sue.giveRaise(0.10)
+print(sue.pay)
