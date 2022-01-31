415b60a68c259ab53b27803b342823c17f087104
冉申 <ranshen0519@icloud.com>
2022-01-31 21:59:51 +0800

    Update other-core-type.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-4/other-core-type.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-4/other-core-type.py
@@ -16,3 +16,20 @@ print(set("spam") - set("ham"))
 print(set("spam") == set("asmp"))
 
 print("p" in set("spam"), "p" in "spam", "ham" in ["eggs", "spam", "ham"])
+
+print(1 / 3)
+print((2 / 3) + (1 / 2))
+
+import decimal
+
+d = decimal.Decimal("3.141")
+print(d + 1)
+
+decimal.getcontext().prec = 2
+print(decimal.Decimal("1.00") / decimal.Decimal("3.00"))
+
+from fractions import Fraction
+
+f = Fraction(2, 3)
+print(f + 1)
+print(f + Fraction(1, 2))
