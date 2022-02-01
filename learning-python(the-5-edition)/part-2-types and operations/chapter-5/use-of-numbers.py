bc8cf349ca05b56340c9135d36aab3b13b8e5a1b
冉申 <ranshen0519@icloud.com>
2022-02-01 22:26:21 +0800

    Update use-of-numbers.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-5/use-of-numbers.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-5/use-of-numbers.py
@@ -156,6 +156,25 @@ print(bin(256), (256).bit_length(), len(bin(256)) - 2)
 import math
 
 print(math.pi, math.e)
+
 print(math.sin(2 * math.pi / 180))
 print(math.sqrt(144), math.sqrt(2))
 print(pow(2, 4), 2**4, 2.0**4.0)
+print(abs(-42.0), sum([1, 2, 3, 4]))
+print(min([3, 1, 2, 4]), max([3, 1, 2, 4]))
+
+print(math.floor(2.567), math.floor(-2.567))
+print(math.trunc(2.567), math.trunc(-2.567))
+print(int(2.567), int(-2.567))
+print(round(2.567), round(2.467), round(2.567, 2))
+print("%.1f" % 2.567, "{0:.2f}".format(2.567))
+
+print((1 / 3.0), round(1 / 3.0, 2), ("%.2f" % (1 / 3.0)))
+
+print(math.sqrt(144))
+print(144**0.5)
+print(pow(144, 0.5))
+
+print(math.sqrt(1234567890))
+print(1234567890**0.5)
+print(pow(1234567890, 0.5))
