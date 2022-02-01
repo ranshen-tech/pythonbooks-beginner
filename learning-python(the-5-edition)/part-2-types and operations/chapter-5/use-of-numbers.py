025d6789e11621f529953f982c6852969fccba97
冉申 <ranshen0519@icloud.com>
2022-02-01 17:57:50 +0800

    Create use-of-numbers.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-5/use-of-numbers.py
@@ -0,0 +1,25 @@
+print(int(3.8))
+print(float(3))
+print(3 == 3.0)
+
+# 变量与基础表达式
+a = 3
+b = 4
+
+print(a + 1, a - 1)
+print(b * 3, b / 2)
+print(a % 2, b**2)
+print(2 + 4.0, 2.0**b)
+
+print(b / 2 + a)
+print(b / (2.0 + a))
+print(b // 2 + a)
+
+
+# 数值的显示格式
+num = 1 / 3.0
+print(num)
+
+print("%e" % num)
+print("%4.2f" % num)
+print("{0:4.2f}".format(num))
