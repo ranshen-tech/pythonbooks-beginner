33b4415e8fa218ada2e9fd72e49d9383941eb04e
冉申 <ranshen0519@icloud.com>
2022-02-02 15:40:09 +0800

    Update fraction-type.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-5/fraction-type.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-5/fraction-type.py
@@ -1,5 +1,7 @@
 # 分数基础知识
+import decimal
 from fractions import Fraction
+from decimal import Decimal
 
 x = Fraction(1, 3)
 y = Fraction(4, 6)
@@ -26,4 +28,24 @@ print(a + b)
 print(a - b)
 print(a * b)
 
-print(1 / 3 + 2 / 3)
+print(0.1 + 0.1 + 0.1 - 0.3)
+print(Fraction(1, 10) + Fraction(1, 10) + Fraction(1, 10) - Fraction(3, 10))
+print(Fraction(1, 10) * 3 - Fraction(3, 10))
+
+print(Decimal("0.1") + Decimal("0.1") + Decimal("0.1") - Decimal("0.3"))
+print(Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3))
+
+print(Decimal("0.1") * 3 - Decimal("0.3"))
+
+print(1 / 3)
+print(Fraction(1, 3))
+decimal.getcontext().prec = 2
+print(Decimal(1) / Decimal(3))
+
+print((1 / 3) + (6 / 12))
+print(Fraction(6, 12))
+
+print(Fraction(1, 3) + Fraction(6, 12))
+
+print(Decimal(str(1 / 3)) + Decimal(str(6 / 12)))
+print(Fraction(1000, 12345677890))
