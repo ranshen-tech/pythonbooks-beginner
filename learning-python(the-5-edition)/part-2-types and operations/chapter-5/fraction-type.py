15015bd21f0b55318f089a794c4c3cd1b88a27fc
冉申 <ranshen0519@icloud.com>
2022-02-02 14:38:03 +0800

    Create fraction-type.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-5/fraction-type.py
@@ -0,0 +1,19 @@
+# 分数基础知识
+from fractions import Fraction
+
+x = Fraction(1, 3)
+y = Fraction(4, 6)
+
+print(x)
+print(y)
+
+print(x + y)
+print(x - y)
+print(x * y)
+
+print(Fraction("0.25"))
+print(Fraction("1.25"))
+print(Fraction("0.25") + Fraction("1.25"))
+
+
+# 分数和小数中的数值精度
