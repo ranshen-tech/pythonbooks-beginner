84b32a9f8069669a728a5a979cb08169bb3aae88
冉申 <ranshen0519@icloud.com>
2022-02-04 16:07:12 +0800

    Update strings-in-practical-use.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-7/strings-in-practical-use.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-7/strings-in-practical-use.py
@@ -27,3 +27,36 @@ S = "abcdefghijklmnop"
 print(S[1:10:2])
 print(S[::2])
 print("\n")
+
+S = "hello"
+print(S[::-1])
+print("\n")
+
+S = "abcdefg"
+print(S[5:1:-1])
+print("\n")
+
+print("spam"[1:3])
+print("spam"[slice(1, 3)])
+print("spam"[::-1])
+print("spam"[slice(None, None, -1)])
+print("\n")
+
+
+# 字符串转换工具
+# print("42" + 1)
+print(int("42"), str(42))
+print(repr(42))
+print(len(repr(42)))
+print(type(repr(42)))
+print("\n")
+
+print(str("spam"), repr("spam"))
+print("\n")
+
+S = "42"
+I = 1
+print(int(S) + I)
+
+print(S + str(I))
+print("\n")
