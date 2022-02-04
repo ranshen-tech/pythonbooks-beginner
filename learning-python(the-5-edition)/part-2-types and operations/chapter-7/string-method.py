66352bb222339fcdf4a1c8f4af056b8fc670432e
冉申 <ranshen0519@icloud.com>
2022-02-04 20:10:27 +0800

    Update string-method.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-7/string-method.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-7/string-method.py
@@ -2,3 +2,30 @@
 S = "spam"
 result = S.find("pa")
 print(result)
+print("\n")
+
+# 修改字符串 ||
+S = "spammy"
+S = S[:3] + "xx" + S[5:]
+print(S)
+print("\n")
+
+S = "spammy"
+S = S.replace("mm", "xx")
+print(S)
+print("\n")
+
+print("aa$bb$cc$dd".replace("$", "SPAM"))
+print("\n")
+
+S = "xxxxSPAMxxxxSPAMxxxx"
+where = S.find("SPAM")
+print(where)
+S = S[:where] + "EGGS" + S[(where + 4) :]
+print(S)
+print("\n")
+
+S = "xxxxSPAMxxxxSPAMxxxx"
+print(S.replace("SPAM", "EGGS"))
+print(S)
+print(S.replace("SPAM", "EGGS", 1))
