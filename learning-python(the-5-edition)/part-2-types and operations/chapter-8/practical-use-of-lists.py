5fc2d75cc882f266c1e263eb58d81d2e13abfbea
冉申 <ranshen0519@icloud.com>
2022-02-07 20:22:23 +0800

    Create practical-use-of-lists.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-8/practical-use-of-lists.py
@@ -0,0 +1,31 @@
+# 基本列表操作
+print(len([1, 2, 3]))
+print([1, 2, 3] + [4, 5, 6])
+print(["Ni!"] * 4)
+print("\n")
+
+print(str([1, 2]) + "34")
+print([1, 2] + list("34"))
+print("\n")
+
+
+# 列表迭代和推导
+print(3 in [1, 2, 3])
+for x in [1, 2, 3]:
+    print(x, end=" ")
+print("\n")
+
+res = [c * 4 for c in "SPAM"]
+print(res)
+print("\n")
+
+res = []
+for c in "SPAM":
+    res.append(c * 4)
+print(res)
+print("\n")
+
+print(list(map(abs, [-1, -2, 0, 1, 2])))
+print("\n")
+
+# 索引、分片和矩阵
