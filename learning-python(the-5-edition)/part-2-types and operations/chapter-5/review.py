d1ac38dcb7d23f4f9bf1136d6a09b24a03ac0a3c
冉申 <ranshen0519@icloud.com>
2022-02-12 19:41:01 +0800

    Update review.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-5/review.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-5/review.py
@@ -14,3 +14,9 @@ print("\n")
 print(math.pi, math.e)
 print(math.sin(2 * math.pi / 180))
 print("\n")
+
+print(math.sqrt(144), math.sqrt(2))
+print("\n")
+
+print(pow(2, 4))
+print(abs(-42.0), sum([1, 2, 3, 4]))
