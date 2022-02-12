027369e09b6d0b77b183af88f8eaa29a1277962d
冉申 <ranshen0519@icloud.com>
2022-02-12 11:08:13 +0800

    Update review.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-4/review.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-4/review.py
@@ -32,3 +32,28 @@ print("\n")
 
 print("%.2f | %05d" % (3.14159, -42))
 print("\n")
+
+# 通用操作符都是以内置函数或表达式的形式出现；类型特定的操作是以方法调用的形式出现
+
+print(s)
+print(dir(s))
+print("\n")
+
+# __add__方法避免使用（速度慢）
+print(s + "Ni!")
+print(s.__add__("Ni!"))
+print("\n")
+
+# print(help(s.isalpha))
+a = "bac3"
+print(a.isalpha())
+print("\n")
+
+# print(help(s.replace))
+print("spam".encode())
+print("spam".encode("utf-8"))
+print("spam".encode().decode())
+print("\n")
+
+print("x" + b"y".decode())
+print("x".encode() + b"y")
