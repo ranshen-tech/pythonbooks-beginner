7b440fd6e169a73bfd4dfebadb9ccdcbd8d1f997
冉申 <ranshen0519@icloud.com>
2022-02-13 10:25:34 +0800

    Update review.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-6/review.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-6/review.py
@@ -31,3 +31,36 @@ l2 = l1[:]
 l1[0] = 24
 print(l1)
 print(l2)
+print("\n")
+
+l1 = [2, 3, 4]
+l2 = list(l1)
+print(l2)
+l1[0] = 24
+print(l1)
+print(l2)
+print("\n")
+
+l1 = [2, 3, 4]
+l2 = l1.copy()
+print(l1)
+print(l2)
+l1[0] = 24
+print(l1)
+print(l2)
+print("\n")
+
+s1 = set("spam")
+print(s1)
+s2 = s1.copy()
+print(s2)
+print("\n")
+
+d1 = {"a": 1, "b": 2}
+print(d1)
+d2 = d1.copy()
+print(d2)
+d1["a"] = 3
+print(d1)
+print(d2)
+print("\n")
