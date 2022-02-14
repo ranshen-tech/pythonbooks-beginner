d34794c547d31aee49a03dc7a3bc544917af85e1
冉申 <ranshen0519@icloud.com>
2022-02-14 19:22:02 +0800

    Update practical-application-of-files.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-9/practical-application-of-files.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-9/practical-application-of-files.py
@@ -1,5 +1,48 @@
-bob = {"name": "bob", "age": 40.5, "jobs": ["dev", "mgr"]}
-job, name, age = bob.values()
-print(bob.values())
-print(job, name, age)
-print(name, job)
+myfile = open("myfile.txt", "w")
+# print(help(myfile.write))
+print(myfile.write("hello text file\n"))
+print(myfile.write("goodbye text file\n"))
+myfile.close()
+print("\n")
+
+myfile = open("myfile.txt")
+# print(help(open))
+a = myfile.readline()
+print(a)
+b = myfile.readline()
+print(b)
+print(myfile.readline())
+
+x = open("myfile.txt").read()
+print(x)
+print(open("myfile.txt").read())
+print("\n")
+
+for line in open("myfile.txt"):
+    print(line, end="")
+print("\n")
+
+# data = open("data.bin", "rb").read()
+# print(data)
+# print("\n")
+
+x, y, z = 43, 44, 45
+s = "spam"
+d = {"a": 1, "b": 2}
+l = [1, 2, 3]
+
+f = open("datafile.txt", "w")
+f.write(s + "\n")
+f.write("%s,%s,%s\n" % (x, y, z))
+f.write(str(l) + "$" + str(d) + "\n")
+f.close()
+
+chars = open("datafile.txt").read()
+print(chars)
+print("\n")
+
+f = open("datafile.txt")
+line = f.readline()
+print(line)
+print(line.rstrip())
+print("\n")
