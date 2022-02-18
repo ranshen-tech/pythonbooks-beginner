17a639f582fc5ec84cd171220a43eefe0125c5dd
冉申 <ranshen0519@icloud.com>
2022-02-18 18:06:54 +0800

    Update syntax-rules.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-12/syntax-rules.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-12/syntax-rules.py
@@ -31,3 +31,84 @@ if a == b and c == d and d == e and f == g:
 if a == b and c == d and d == e and e == f:
     print("new")
 print("\n")
+
+x = 1 + 2 + 3 + 4
+print(x)
+x = 1 + 2 + 3
++4
+print(x)
+print("\n")
+
+x = 1
+y = 2
+print(x)
+print("\n")
+
+S = """
+aaaa
+bbbb
+cccc"""
+print(S)
+
+S = "aaaa" "bbbb" "cccc"
+print(S)
+print("\n")
+
+a, b = "hello", "world"
+print(a, b)
+print("hello" "world")
+print("\n")
+
+if 1:
+    print("hello")
+print("\n")
+
+
+# 真值和布尔测试
+print(2 < 3, 3 < 2)
+print("\n")
+
+print(2 or 3, 3 or 2)
+print([] or 3)
+print([] or {})
+print({} or [])
+print("\n")
+
+print(2 and 3, 3 and 2)
+print([] and {})
+print(3 and [])
+print("\n")
+
+
+# if/else 三元表达式
+X = 1
+Y = 2
+Z = 3
+if X:
+    A = Y
+else:
+    A = Z
+print(A)
+print("\n")
+
+A = Y if X else Z
+
+A = "t" if "spam" else "f"
+print(A)
+A = "t" if "" else "f"
+print(A)
+A = Y if X else Z
+print("\n")
+
+print(["f", "t"][bool("")])
+print(["f", "t"][bool("spam")])
+print("\n")
+
+# filter函数调用和列表推导
+L = [1, 0, 2, 0, "spam", "", "ham", []]
+print(list(filter(bool, L)))
+print([x for x in L if x])
+print(any(L))
+print(all(L))
+# print(help(any))
+# print(help(all(L)))
