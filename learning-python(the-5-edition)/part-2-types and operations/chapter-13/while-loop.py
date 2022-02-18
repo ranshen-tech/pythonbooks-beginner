69f804ba5977ae5d08051437a14c79307469b5db
冉申 <ranshen0519@icloud.com>
2022-02-18 19:34:32 +0800

    Create while-loop.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-13/while-loop.py
@@ -0,0 +1,45 @@
+# 示例
+# while True:
+# print("Type Ctrl-C to stop me!")
+
+x = "spam"
+while x:
+    print(x, end=" ")
+    x = x[1:]
+print("\n")
+
+a, b = 0, 10
+while a < b:
+    print(a, end=" ")
+    a += 1
+print("\n")
+
+# while True:
+# pass
+# if exitTest():
+# break
+
+
+# pass
+
+# while True:
+# pass
+
+
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+
+def func1():
+    ...
+
+
+def func2():
+    ...
+
+
+func1()
