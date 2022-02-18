906b9bea776a1a79f743fd9e721392a428cb6974
冉申 <ranshen0519@icloud.com>
2022-02-18 09:41:17 +0800

    Update expression-statement.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-11/expression-statement.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-11/expression-statement.py
@@ -33,3 +33,29 @@ print(x, y, z, end="")
 print(x, y, z)
 print(x, y, z, end="...\n")
 print("\n")
+
+print(x, y, z, sep="...", end="!\n")
+print(x, y, z, end="!\n", sep="...")
+print("\n")
+
+print(x, y, z, sep="...", file=open("data.txt", "w"))
+print(x, y, z)
+print(open("data.txt").read())
+print("\n")
+
+text = "%s: %-.4f, %05d" % ("Result", 3.14159, 42)
+print(text)
+print("%s: %-.4f, %05d" % ("Result", 3.14159, 42))
+print("\n")
+
+
+# 打印流重定向
+print("hello world")
+print("\n")
+
+import sys
+
+sys.stdout.write("hello world\n")
+print("\n")
+
+# 手动定向输出流
