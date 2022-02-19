c1075622821058f0f9eb3da66dffc23e00217ae7
冉申 <ranshen0519@icloud.com>
2022-02-19 10:06:45 +0800

    Create for-loop.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-13/for-loop.py
@@ -0,0 +1,31 @@
+# Example
+for x in ["spam", "eggs", "ham"]:
+    print(x, end=" ")
+print("\n")
+
+sum = 0
+for x in [1, 2, 3, 4]:
+    sum += x
+print(sum)
+
+prod = 1
+for item in [1, 2, 3, 4]:
+    prod *= item
+print(prod)
+print("\n")
+
+
+# Other data types
+S = "lumberjack"
+T = ("and", "I'm", "okay")
+
+for x in S:
+    print(x, end=" ")
+print("\n")
+
+for x in T:
+    print(x, end=" ")
+print("\n")
+
+
+# Tuple assignment in the for loop
