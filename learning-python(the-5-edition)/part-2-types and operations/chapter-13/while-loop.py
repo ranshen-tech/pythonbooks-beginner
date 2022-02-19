90f79cdeccd2899e7f0820b3e7e3b13589f34afb
冉申 <ranshen0519@icloud.com>
2022-02-19 08:57:02 +0800

    Update while-loop.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-13/while-loop.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-13/while-loop.py
@@ -61,3 +61,68 @@ print("\n")
 
 
 # continue
+x = 10
+while x:
+    x -= 1
+    if x % 2 != 0:
+        continue
+    print(x, end=" ")
+print("\n")
+
+x = 10
+while x:
+    x -= 1
+    if x % 2 == 0:
+        print(x, end=" ")
+print("\n")
+
+
+# break
+
+# while True:
+#     name = input("Enter name:")
+#     if name == "stop":
+#         break
+#     age = input("Enter age: ")
+#     print("Hello", name, "=>", int(age) ** 2)
+
+
+# 循环的else
+y = 4
+x = y // 2
+while x > 1:
+    if y % x == 0:
+        print(y, "has factor", x)
+        break
+    x -= 1
+else:
+    print(y, "is prime")
+print("\n")
+
+found = False
+# while x and not found:
+#     if match(x[0]):
+#         print("Ni")
+#         found = True
+#     else:
+#         x = x[1:]
+# if not found:
+# print("not found")
+
+# while x:
+#     if match(x[0]):
+#         print("Ni")
+#         break
+#     x = x[1:]
+# else:
+#     print("Not found")
+
+x = []
+while x:
+    if x[0]:
+        print("Ni")
+        break
+    x = x[1:]
+else:
+    print("Not found")
+print("hello")
