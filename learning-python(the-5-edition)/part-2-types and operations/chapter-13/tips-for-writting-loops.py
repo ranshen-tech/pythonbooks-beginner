685513934c7e7c10d5fda0aeb876ed0a40fac49a
冉申 <ranshen0519@icloud.com>
2022-02-19 18:43:22 +0800

    Update tips-for-writting-loops.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-13/tips-for-writting-loops.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-13/tips-for-writting-loops.py
@@ -56,3 +56,37 @@ L = [1, 2, 3]
 for i in range(len(L)):
     X = L[i:] + L[:i]
     print(X, end=" ")
+print("\n")
+
+
+# 非穷尽遍历：range VS slice
+S = "abcdefghijk"
+print(list(range(0, len(S), 2)))
+print("\n")
+
+for i in range(0, len(S), 2):
+    print(S[i], end=" ")
+print("\n")
+
+S = "abcdefghijk"
+for c in S[::2]:
+    print(c, end=" ")
+print("\n")
+
+
+# 修改list：range vs 推导
+L = [1, 2, 3, 4, 5]
+
+for x in L:
+    x += 1
+
+print(L)
+print(x)
+print("\n")
+
+L = [1, 2, 3, 4, 5]
+
+for i in range(len(L)):
+    L[i] += 1
+
+print(L)
