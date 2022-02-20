3e693328e7431534779060838f1d35121632a5ef
冉申 <ranshen0519@icloud.com>
2022-02-20 11:58:58 +0800

    Update iteration.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-14/iteration.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-14/iteration.py
@@ -35,3 +35,54 @@ print(f.__next__().strip())
 print(f.__next__().strip())
 # print(f.__next__().strip())
 print("💖")
+
+for line in open("script2.py"):
+    print(line.upper(), end="")
+print("\n")
+
+for line in open("script2.py").readlines():
+    print(line.upper(), end="")
+print("\n")
+
+f = open("script2.py")
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(line, end="")
+print("\n💖")
+
+
+# Manual iteration: iter & next
+
+f = open("script2.py")
+print(f.__next__().rstrip())
+print(f.__next__().rstrip())
+print("💖")
+
+f = open("script2.py")
+print(next(f).rstrip())
+print(next(f).rstrip())
+print("💖")
+
+
+# Complete iteration agreement
+
+L = [1, 2, 3]
+I = iter(L)
+print(I)
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
+print("\n")
+
+f = open("script2.py")
+print(iter(f) is f)
+print(iter(f) is f.__iter__())
+print(f.__next__())
+print(next(f))
+print("\💖")
+
+L = [1, 2, 3]
+print(iter(L) is L)
+print(L.__next__())
