2e2ba7388bb2eb2f87fd87570b27e3feec31670f
冉申 <ranshen0519@icloud.com>
2022-02-20 10:11:22 +0800

    Update iteration.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-14/iteration.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-14/iteration.py
@@ -1,10 +1,37 @@
-for line in open("log.txt").readlines():
-    print(line.rstrip())
+# Iterator
+
+for x in [1, 2, 3, 4]:
+    print(x**2, end=" ")
 print("\n")
 
-for line in open("log.txt"):
-    print(line)
-print("\n--")
+for x in (1, 2, 3, 4):
+    print(x**3, end=" ")
+print("\n")
 
-for line in reversed(open("log.txt")):
-    print(line)
+for x in "spam":
+    print(x * 2, end=" ")
+print("\n")
+
+
+# Iterative agreement: File iterator
+
+print(open("script2.py").read())
+print("💖")
+
+f = open("script2.py")
+print(f.readline().rstrip())
+print(f.readline().rstrip())
+print(f.readline().rstrip())
+print(f.readline().rstrip())
+print(f.readline().rstrip())
+print(f.readline().rstrip())
+print("💘")
+
+f = open("script2.py")
+print(f.__next__().strip())
+print(f.__next__().strip())
+print(f.__next__().strip())
+print(f.__next__().strip())
+print(f.__next__().strip())
+# print(f.__next__().strip())
+print("💖")
