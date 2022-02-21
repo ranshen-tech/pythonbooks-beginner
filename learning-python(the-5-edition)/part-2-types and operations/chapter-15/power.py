a5f9e5295b7bd5d5655424dfb77cc5f40250e6f0
冉申 <ranshen0519@icloud.com>
2022-02-21 21:00:30 +0800

    Update power.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-15/power.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-15/power.py
@@ -1,15 +1,23 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-found = False
-i = 0
-while not found and i < len(L):
-    if 2**X == L[i]:
-        found = True
-    else:
-        i += 1
+# i = 0
+# while i < len(L):
+#     if 2**X == L[i]:
+#         print("at index", i)
+#         break
+#     i += 1
+# else:
+#     print(X, "not found")
 
-if found:
-    print("at index", i)
+# for num in L:
+#     if 2**X == num:
+#         print(L.index(num))
+#         break
+# else:
+#     print("not found")
+
+if 2**X in L:
+    print("found at", L.index(2**X))
 else:
-    print(X, "not found")
+    print("not found")
