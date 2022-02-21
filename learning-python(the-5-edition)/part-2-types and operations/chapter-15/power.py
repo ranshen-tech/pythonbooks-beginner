d843c7242d2b479e7892bb9a9265c53c8f7a9cbd
冉申 <ranshen0519@icloud.com>
2022-02-21 22:15:06 +0800

    Update power.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-15/power.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-15/power.py
@@ -1,23 +1,60 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-# i = 0
-# while i < len(L):
-#     if 2**X == L[i]:
-#         print("at index", i)
-#         break
-#     i += 1
-# else:
-#     print(X, "not found")
+# a
 
-# for num in L:
-#     if 2**X == num:
-#         print(L.index(num))
-#         break
-# else:
-#     print("not found")
+i = 0
+while i < len(L):
+    if 2**X == L[i]:
+        print("at index", i)
+        break
+    i += 1
+else:
+    print(X, "not found")
+print("\n")
+
+# b
+
+for num in L:
+    if 2**X == num:
+        print(2**X, "was found at", L.index(num))
+        break
+else:
+    print(X, "not found")
+print("\n")
+
+# c
 
 if 2**X in L:
-    print("found at", L.index(2**X))
+    print(2**X, "was found at", L.index(2**X))
 else:
-    print("not found")
+    print(X, "not found")
+print("\n")
+
+# d
+
+X = 5
+L = []
+for i in range(7):
+    L.append(2**i)
+print(L)
+print("\n")
+
+# e
+
+if 2**X in L:
+    print(2**X, "was found at", L.index(2**X))
+else:
+    print(X, "not found")
+print("\n")
+
+# f
+
+L = list(map(lambda x: 2**x, range(7)))
+print(L)
+
+if 2**X in L:
+    print(2**X, "was found at", L.index(2**X))
+else:
+    print(X, "not found")
+print("\n")
