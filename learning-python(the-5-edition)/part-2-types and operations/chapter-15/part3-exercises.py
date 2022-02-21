cf3d95166262c215b6e9fc34fc19476fc5835208
冉申 <ranshen0519@icloud.com>
2022-02-21 19:14:15 +0800

    Update part3-exercises.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-15/part3-exercises.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-15/part3-exercises.py
@@ -1,4 +1,5 @@
 # exercise1
+
 S = "spam"
 for c in S:
     print(ord(c))
@@ -17,4 +18,29 @@ print(x)
 print("\n")
 
 print(list(map(ord, S)))
+print("\n")
+
+print([ord(c) for c in S])
+print("\n")
+
+
+# exercise2
+
+for i in range(5):
+    print("hello %d\n\a" % i)
+print("\n")
+
+
+# exercise3
+
+D = {"b": 2, "a": 1, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7}
+keys = list(D)
+print(keys)
+keys.sort()
+for key in keys:
+    print(key, "=>", D[key])
+print("\n")
+
+for key in sorted(D):
+    print(key, "=>", D[key])
 print("\n")
