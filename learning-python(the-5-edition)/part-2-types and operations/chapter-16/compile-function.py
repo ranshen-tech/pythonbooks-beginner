458d429ab46826ce15cc6cfbfbf8bf2ffb66cd9e
冉申 <ranshen0519@icloud.com>
2022-02-21 23:38:16 +0800

    Update compile-function.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-16/compile-function.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16/compile-function.py
@@ -1,4 +1,31 @@
-L = [4, 5, 6]
+# def statement
 
-Y = list(L) * 4
-print(Y)
+def name(arg1, arg2,...argN):
+    statements
+
+
+
+
+def name(arg2, arg2...argN):
+    ...
+    return value
+
+
+if test:
+    def func():
+        ...
+else:
+    def func():
+        ...
+...
+func()
+
+
+othername = func
+othername()
+
+def func():
+    ...
+func()
+func.attr = value
+
