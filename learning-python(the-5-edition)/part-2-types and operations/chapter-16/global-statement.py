7bfc66cc1c01fd63adfd4e11e4c085d0949dccc3
冉申 <ranshen0519@icloud.com>
2022-02-22 21:38:51 +0800

    Update global-statement.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-16/global-statement.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16/global-statement.py
@@ -33,3 +33,40 @@ def func2():
 print(func2())
 print(func1())
 print("\n")
+
+
+def f1():
+    X = 88
+
+    def f2():
+        print(X)
+
+    return f2
+
+
+action = f1()
+action()
+print("\n")
+
+
+# factory function & closure
+
+
+def maker(N):
+    def action(X):
+        return X**N
+
+    return action
+
+
+f = maker(2)
+print(f)
+print(f(3))
+print(f(4))
+print("\n")
+
+g = maker(3)
+print(g(4))
+print(f(4))
+print("\n")
+print(maker(3)(4))
