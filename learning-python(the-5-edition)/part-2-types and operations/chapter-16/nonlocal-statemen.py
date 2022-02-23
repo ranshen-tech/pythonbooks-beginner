728c95b05b278ec744155f69cc65dd3f303cc400
冉申 <ranshen0519@icloud.com>
2022-02-23 18:17:30 +0800

    Update nonlocal-statemen.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-16/nonlocal-statemen.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16/nonlocal-statemen.py
@@ -171,3 +171,20 @@ H = tester(99)
 H("juice")
 H("pancakes")
 print("\n")
+
+
+def tester(start):
+    def nested(label):
+        print(label, nested.state)
+        nested.state += 1
+
+    nested.state = start
+    return nested
+
+
+F = tester(0)
+print(F.state)
+F("spam")
+F("ham")
+print(F.state)
+print("\n")
