b24eca20bae48d107dc7c60e5b4a096f06ba7cda
冉申 <ranshen0519@icloud.com>
2022-02-23 12:47:49 +0800

    Update nonlocal-statemen.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-16/nonlocal-statemen.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16/nonlocal-statemen.py
@@ -1,10 +1,54 @@
-def f2(x):
-    print(x)
+# def func():
+#     nonlocal name1, name2, ...
 
 
-def f1():
-    x = 88
-    f2(x)
+# nonlocal use
 
 
-f1()
+def tester(start):
+    state = start
+
+    def nested(label):
+        print(label, state)
+
+    return nested
+
+
+F = tester(0)
+F("spam")
+F("ham")
+print("\n")
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(0)
+# F("spam")
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        nonlocal state
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(0)
+F("spam")
+F("ham")
+F("eggs")
+
+G = tester(42)
+G("spam")
