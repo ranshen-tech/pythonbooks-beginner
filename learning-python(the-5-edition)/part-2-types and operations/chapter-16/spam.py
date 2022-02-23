6905ecda5820b90a01d05bb0fc18b84c7dfe7e54
冉申 <ranshen0519@icloud.com>
2022-02-23 19:47:17 +0800

    Update spam.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-16/spam.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16/spam.py
@@ -8,3 +8,17 @@ F.read()
 makeopen("eggs")
 F = open("script2.py")
 F.read()
+
+
+import builtins
+
+
+class makeopen:
+    def __init__(self, id):
+        self.id = id
+        self.original = builtins.open
+        builtins.open = self
+
+    def __call__(self, *kargs, **pargs):
+        print("Custom open call %r" % self.id, kargs, pargs)
+        return self.original(*kargs, **pargs)
