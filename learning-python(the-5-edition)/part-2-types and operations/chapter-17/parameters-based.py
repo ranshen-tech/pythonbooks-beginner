4b9020fa87f0f6e8a66989776b768d1e4b8c0739
冉申 <ranshen0519@icloud.com>
2022-02-24 20:09:49 +0800

    Update parameters-based.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-17/parameters-based.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/parameters-based.py
@@ -185,3 +185,16 @@ args += (4,)
 print(args)
 # func(*args)
 print("\n")
+
+
+def tracer(func, *pargs, **kargs):
+    print("calling:", func.__name__)
+    return func(*pargs, **kargs)
+
+
+def func(a, b, c, d):
+    return a + b + c + d
+
+
+print(tracer(func, 1, 2, c=3, d=4))
+print("\n")
