3f82b8a385e9f6e44392f863d37ce945464780d8
冉申 <ranshen0519@icloud.com>
2022-02-24 19:08:35 +0800

    Update parameters-based.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-17/parameters-based.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/parameters-based.py
@@ -1,23 +1,67 @@
-def tester(start):
-    def nested(label):
-        print(label, nested.state)
-        nested.state += 1
-
-    nested.state = start
-    return nested
+# 参数和共享引用
 
 
-F = tester(0)
-print(F.state)
-F("spam")
-F("ham")
-print(F.state)
+def f(a):
+    a = 99
+
+
+b = 88
+print(f(b))
+f(b)
+print(b)
 print("\n")
 
-# G = tester(42)
-# G("eggs")
-# F("ham")
-# F("ham")
-# print(F.state)
-# print(G.state)
-# print(F is G)
+
+def changer(a, b):
+    a = 2
+    b[0] = "spam"
+    return a, b
+
+
+X = 1
+L = [1, 2]
+changer(X, L)
+print(X, L)
+print("\n")
+
+X = 1
+a = X
+a = 2
+print(X)
+print("\n")
+
+L = [1, 2]
+b = L
+b[0] = "spam"
+print(L)
+print("\n")
+
+
+# 避免修改可变参数
+L = [1, 2]
+
+
+def changer(a, b):
+    b = b[:]
+    a = 2
+    b[0] = "spam"
+
+
+# changer(X, tuple(L))
+print("\n")
+
+
+# 模拟输出参数和多重结果
+
+
+def multiple(x, y):
+    x = 2
+    y = [3, 4]
+    return x, y
+
+
+X = 1
+L = [1, 2]
+X, L = multiple(X, L)
+print(X, L)
+print("\n")
