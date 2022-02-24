469c8796d0af64a2f4bb4e4867b7d1672b28a4a5
冉申 <ranshen0519@icloud.com>
2022-02-24 21:47:06 +0800

    Update keyword-only.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-17/keyword-only.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/keyword-only.py
@@ -27,5 +27,26 @@ keywordonly(1)
 keywordonly(1, c=3)
 keywordonly(a=1)
 keywordonly(c=3, b=2, a=1)
-keywordonly(1, 2)
+# keywordonly(1, 2)
+print("\n")
+
+
+def kwonly(a, *, b, c="spam"):
+    print(a, b, c)
+
+
+kwonly(1, b="eggs")
+# kwonly(1, c="eggs")
+# kwonly(1, 2)
+print("\n")
+
+
+def kwonly(a, *, b=1, c, d=2):
+    print(a, b, c, d)
+
+
+kwonly(3, c=4)
+kwonly(3, c=4, b=5)
+# kwonly(3)
+# kwonly(1, 2, 3)
 print("\n")
