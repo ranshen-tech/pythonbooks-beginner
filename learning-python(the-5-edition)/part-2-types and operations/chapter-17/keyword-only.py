7cd5888dd890aee6e5861bfdcb33116e59a2c1d6
冉申 <ranshen0519@icloud.com>
2022-02-24 23:07:19 +0800

    Update keyword-only.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-17/keyword-only.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/keyword-only.py
@@ -50,3 +50,29 @@ kwonly(3, c=4, b=5)
 # kwonly(3)
 # kwonly(1, 2, 3)
 print("\n")
+
+
+# def kwonly(a, **pargs, b, c):
+# def kwonly(a, **, b, c):
+
+# def f(a, *b, **d, c=6):
+# print(a,b, c,d)
+
+
+def f(a, *b, c=6, **d):
+    print(a, b, c, d)
+
+
+f(1, 2, 3, x=4, y=5)
+f(1, 2, 3, x=4, y=5, c=7)
+f(1, 2, 3, c=7, x=4, y=5)
+print("\n")
+
+
+def f(a, c=6, *b, **d):
+    print(a, b, c, d)
+
+
+f(1, 2, 3, 4)
+f(1, 2, 3, x=4)
+print("\n")
