1dba4bd6b03dde73032ee102c591cef10d950852
冉申 <ranshen0519@icloud.com>
2022-03-01 11:43:08 +0800

    Update set-function.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-17/set-function.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/set-function.py
@@ -8,3 +8,24 @@ print(intersect([1, 2, 3], (1, 4)))
 print(intersect(s1, s2, s3))
 print(union(s1, s2, s3))
 print("\n")
+
+
+def tester(func, items, trace=True):
+    for i in range(len(items)):
+        items = items[1:] + items[:1]
+        if trace:
+            print(items)
+        print(sorted(func(*items)))
+
+
+tester(intersect, ("a", "abcdefg", "abdst", "albmacnd"))
+print("\n")
+tester(union, ("a", "abcdefg", "abdst", "albmcnd"), False)
+print("\n")
+tester(intersect, ("ba", "abcdefg", "abdst", "albmcnd"), False)
+print("\n")
+
+
+print(intersect([1, 2, 1, 3], (1, 1, 4)))
+print(union([1, 2, 1, 3], (1, 1, 4)))
+tester(intersect, ("ababa", "abcdefga", "aaaab"), False)
