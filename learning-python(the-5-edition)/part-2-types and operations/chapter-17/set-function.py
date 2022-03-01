5ca79e55738f7badfff907b19cfbe1ca859b7058
冉申 <ranshen0519@icloud.com>
2022-03-01 11:08:45 +0800

    Update set-function.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-17/set-function.py
@@ -0,0 +1,10 @@
+from inter2 import intersect, union
+
+s1, s2, s3 = "SPAM", "SCAM", "SLAM"
+
+print(intersect(s1, s2))
+print(union(s1, s2))
+print(intersect([1, 2, 3], (1, 4)))
+print(intersect(s1, s2, s3))
+print(union(s1, s2, s3))
+print("\n")
