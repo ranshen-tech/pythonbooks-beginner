a48017c03d1f465fd253533564a3ef701bb8219a
冉申 <ranshen0519@icloud.com>
2022-03-05 07:24:11 +0800

    Create makeopen.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16-17/makeopen.py
@@ -0,0 +1,15 @@
+import builtins
+
+
+def makeopen(id):
+    original = builtins.open
+
+    def custom(*kargs, **pargs):
+        print("Custom open call %r:" % id, kargs, pargs)
+        return original(*kargs, **pargs)
+
+    builtins.open = custom
+
+
+F = open("script2.py")
+print(F.read())
