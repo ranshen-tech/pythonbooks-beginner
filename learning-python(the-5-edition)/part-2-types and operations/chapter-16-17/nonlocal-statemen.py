411eaf5bccc734b77ed9369a2dc57c1cb6c9b08f
冉申 <ranshen0519@icloud.com>
2022-03-05 07:24:15 +0800

    Create nonlocal-statemen.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-16-17/nonlocal-statemen.py
@@ -0,0 +1,221 @@
+# def func():
+#     nonlocal name1, name2, ...
+
+
+# nonlocal use
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        print(label, state)
+
+    return nested
+
+
+F = tester(0)
+F("spam")
+F("ham")
+print("\n")
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(0)
+# F("spam")
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        nonlocal state
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(0)
+F("spam")
+F("ham")
+F("eggs")
+print("\n")
+
+G = tester(42)
+G("spam")
+G("eggs")
+F("bacon")
+print("\n")
+
+
+def tester(start):
+    def nested(label):
+        global state
+        state = 0
+        print(label, state)
+
+    return nested
+
+
+F = tester(0)
+F("abc")
+print(state)
+
+C = tester(3)
+C("ranshen")
+print("\n")
+
+spam = 99
+
+
+def tester():
+    def nested():
+        global spam
+        print("Current=", spam)
+        spam += 1
+
+    return nested
+
+
+tester()()
+tester()()
+tester()()
+print("\n")
+
+
+# nonlocal state of variable(变量的状态)
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        nonlocal state
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(78)
+F("spam")
+# print(F.state)
+print("\n")
+
+
+def tester(start):
+    global state
+    state = start
+
+    def nested(label):
+        global state
+        print(label, state)
+        state += 1
+
+    return nested
+
+
+F = tester(9)
+F("spam")
+F("eggs")
+
+G = tester(42)
+G("toast")
+G("bacon")
+F("ham")
+print("\n")
+
+
+class tester:
+    def __init__(self, start):
+        self.state = start
+
+    def nested(self, label):
+        print(label, self.state)
+        self.state += 1
+
+
+F = tester(0)
+F.nested("spam")
+F.nested("ham")
+print("\n")
+
+G = tester(42)
+G.nested("toast")
+G.nested("bacon")
+F.nested("eggs")
+print(F.state)
+print("\n")
+
+
+class tester:
+    def __init__(self, start):
+        self.state = start
+
+    def __call__(self, label):
+        print(label, self.state)
+        self.state += 1
+
+
+H = tester(99)
+H("juice")
+H("pancakes")
+print("\n")
+
+
+def tester(start):
+    def nested(label):
+        print(label, nested.state)
+        nested.state += 1
+
+    nested.state = start
+    return nested
+
+
+F = tester(0)
+print(F.state)
+F("spam")
+F("ham")
+print(F.state)
+print("\n")
+
+G = tester(42)
+G("eggs")
+F("ham")
+F("ham")
+print(F.state)
+print(G.state)
+print(F is G)
+print("\n")
+
+
+# 可变对象的状态
+
+
+def tester(start):
+    state = [start]
+
+    def nested(label):
+        print(label, state[0])
+        state[0] += 1
+
+    # state = [start]
+    return nested
+
+
+F = tester(0)
+F("spam")
+F("ham")
+G = tester(9)
+G("ranshen")
+F("xupeng")
