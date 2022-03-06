25c32868fe807ed34f023e54fb0b6cde0fae1500
冉申 <ranshen0519@icloud.com>
2022-03-06 13:00:16 +0800

    Update lambda.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-19/lambda.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-19/lambda.py
@@ -32,3 +32,36 @@ msg = act("robin")
 print(msg)
 print(act)
 print("\n")
+
+
+# 为什么使用lambda
+
+
+L = [lambda x: x**2, lambda x: x**3, lambda x: x**4]
+
+for f in L:
+    print(f(2))
+
+print(L[0](3))
+print("\n")
+
+
+def f1(x):
+    return x**2
+
+
+def f2(x):
+    return x**3
+
+
+def f3(x):
+    return x**4
+
+
+L = [f1, f2, f3]
+
+for f in L:
+    print(f(2))
+
+print(L[0](3))
+print("\n")
