bb93cbe9535e57e28b69038b603fb5f3d1c96d09
冉申 <ranshen0519@icloud.com>
2022-03-07 10:23:52 +0800

    Update comprehension&functiontools.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension&functiontools.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension&functiontools.py
@@ -55,6 +55,7 @@ print("\n")
 
 res = [x + y for x in [0, 1, 2] for y in [100, 200, 300]]
 print(res)
+print("\n")
 
 res = []
 for x in [0, 1, 2]:
@@ -62,4 +63,45 @@ for x in [0, 1, 2]:
         res.append(x + y)
 
 print(res)
+print("\n")
+
+print([x + y for x in "spam" for y in "SPAM"])
+print("\n")
+
+print([x + y for x in "spam" if x in "sm" for y in "SPAM" if y in ("P", "A")])
+print("\n")
+
+print(
+    [
+        x + y + z
+        for x in "spam"
+        if x in "sm"
+        for y in "SPAM"
+        if y in ("P", "A")
+        for z in "123"
+        if z > "1"
+    ]
+)
+print("\n")
+
+print([(x, y) for x in range(5) if x % 2 == 0 for y in range(5) if y % 2 == 1])
+print("\n")
+
+res = []
+for x in range(5):
+    if x % 2 == 0:
+        for y in range(5):
+            if y % 2 == 1:
+                res.append((x, y))
+print(res)
+print("\n")
+
+
+# example: comprehension & 矩阵
+
+M = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+N = [[2, 2, 2], [3, 3, 3], [4, 4, 4]]
+
+print(M[1])
+print(M[1][2])
 print("\n")
