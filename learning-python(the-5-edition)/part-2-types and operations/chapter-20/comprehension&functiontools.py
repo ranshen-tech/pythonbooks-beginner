d51caadd90598a94d4b392f588849a45a9a6f894
冉申 <ranshen0519@icloud.com>
2022-03-07 10:03:23 +0800

    Update comprehension&functiontools.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension&functiontools.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension&functiontools.py
@@ -38,3 +38,28 @@ for x in range(5):
 
 print(res)
 print("\n")
+
+print([x**2 for x in range(10) if x % 2 == 0])
+
+print(list(map(lambda x: x**2, filter(lambda x: x % 2 == 0, range(10)))))
+print("\n")
+
+
+# standard comprehension grammar
+
+# [expression for target in iterable]
+
+# [expression for target1 in iterable1 if condition1
+#             for target2 in iterable2 if condition2 ...
+#             for targetN in iterableN if conditionN]
+
+res = [x + y for x in [0, 1, 2] for y in [100, 200, 300]]
+print(res)
+
+res = []
+for x in [0, 1, 2]:
+    for y in [100, 200, 300]:
+        res.append(x + y)
+
+print(res)
+print("\n")
