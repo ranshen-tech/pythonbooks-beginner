756308328ce871b96c6e5da4bb269d97e38fd1c0
冉申 <ranshen0519@icloud.com>
2022-03-08 15:16:23 +0800

    Create value generation.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/value generation.py
@@ -0,0 +1,49 @@
+# 内置类型、工具和类中的值生成
+
+D = {"a": 1, "b": 2, "c": 3}
+x = iter(D)
+print(next(x))
+print(next(x))
+print(next(x))
+print("\n")
+
+
+for key in D:
+    print(key, D[key])
+print("\n")
+
+
+for line in open("temp.txt"):
+    print(line, end="")
+print("\n")
+
+
+# 生成器和库工具： 目录遍历器
+
+import os
+
+for (root, subs, files) in os.walk("."):
+    for name in files:
+        if name.startswith("call"):
+            print(root, name)
+print("\n")
+
+
+G = os.walk(r"C:\code\pkg")
+print(iter(G) is G)
+I = iter(G)
+# print(next(I))
+print("\n")
+
+
+# 生成器和函数应用
+
+
+def f(a, b, c):
+    print("%s, %s, and %s" % (a, b, c))
+
+
+f(0, 1, 2)
+f(*range(3))
+f(*(i for i in range(3)))
+print("\n")
