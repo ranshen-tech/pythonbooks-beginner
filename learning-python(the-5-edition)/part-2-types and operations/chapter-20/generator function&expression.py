cd03086803eea4b85459177f08c02208faae28e2
冉申 <ranshen0519@icloud.com>
2022-03-08 09:29:45 +0800

    Update generator function&expression.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/generator function&expression.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/generator function&expression.py
@@ -40,11 +40,59 @@ for x in buildsquares(5):
     print(x, end=" : ")
 print("\n")
 
-
 for x in [n**2 for n in range(5)]:
     print(x, end=" : ")
 print("\n")
 
 for x in map(lambda n: n**2, range(5)):
     print(x, end=" : ")
+print("\n")
+
+
+def ups(line):
+    for sub in line.split(","):
+        yield sub.upper()
+
+
+print(tuple(ups("aaa,bbb,ccc")))
+print({i: s for i, s in enumerate(ups("aaa,bbb,ccc"))})
+print("\n")
+
+
+# 扩展生成器函数协议：send VS next
+
+
+def gen():
+    for i in range(10):
+        X = yield i
+        print(X)
+
+
+G = gen()
+print(next(G))
+print(G.send(77))
+print(G.send(88))
+print(next(G))
+print("\n")
+
+
+# 生成器表达式：当可迭代对象遇见推导语法
+
+print([x**2 for x in range(4)])
+print(x**2 for x in range(4))
+print((x**2 for x in range(4)))
+
+print(list(x**2 for x in range(4)))
+print("\n")
+
+
+G = (x**2 for x in range(4))
+print(G)
+print(iter(G))
+print(iter(G) is G)
+print(next(G))
+print(next(G))
+print(next(G))
+print(next(G))
+print(G)
 print("\n")
