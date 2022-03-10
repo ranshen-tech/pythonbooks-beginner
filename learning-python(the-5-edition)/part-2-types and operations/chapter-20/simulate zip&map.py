1c60f4bd61ba049d1c5ed4cb29c26a74df0ed09f
冉申 <ranshen0519@icloud.com>
2022-03-10 21:43:32 +0800

    Update simulate zip&map.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/simulate zip&map.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/simulate zip&map.py
@@ -24,3 +24,27 @@ def mymap(func, *seqs):
     for args in zip(*seqs):
         res.append(func(*args))
     return res
+
+
+print(mymap(abs, [-2, -1, 0, 1, 2]))
+print(mymap(pow, [1, 2, 3], [2, 3, 4, 5]))
+print("\n")
+
+
+def mymap(func, *seqs):
+    return [func(*args) for args in zip(*seqs)]
+
+
+print(mymap(abs, [-2, -1, 0, 1, 2]))
+print(mymap(pow, [1, 2, 3], [2, 3, 4, 5]))
+print("\n")
+
+
+def mymap(func, *seqs):
+    res = []
+    for args in zip(*seqs):
+        yield func(*args)
+
+
+def mymap(func, *seqs):
+    return (func(*args) for args in zip(*seqs))
