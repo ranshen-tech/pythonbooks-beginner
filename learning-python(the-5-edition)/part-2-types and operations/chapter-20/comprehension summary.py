16d893c72ceac91c63e86d53da4171a94043c215
冉申 <ranshen0519@icloud.com>
2022-03-14 08:50:17 +0800

    Create comprehension summary.py

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension summary.py
@@ -0,0 +1,40 @@
+print([x * x for x in range(10)])
+
+print((x * x for x in range(10)))
+
+print({x * x for x in range(10)})
+
+print({x: x * x for x in range(10)})
+print("\n")
+
+
+# action scope及推导变量
+
+print(x for x in range(5))
+print("\n")
+
+x = 99
+print([x for x in range(5)])
+print(x)
+print("\n")
+
+y = 99
+for y in range(5):
+    pass
+print(y)
+print("\n")
+
+
+x = "aaa"
+
+
+def func():
+    y = "bbb"
+    print(z for z in x + y)
+    print("".join(z for z in x + y))
+
+
+func()
+print("\n")
+
+print(x for x in range(5))
