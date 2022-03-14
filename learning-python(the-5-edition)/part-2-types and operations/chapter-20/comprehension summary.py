e09501bdc8a25324f4fd376bc3c7ef5d767bc5cf
冉申 <ranshen0519@icloud.com>
2022-03-14 10:29:12 +0800

    Update comprehension summary.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension summary.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension summary.py
@@ -62,3 +62,50 @@ print("\n")
 print({x: x * x for x in range(10)})
 print(dict((x, x * x) for x in range(10)))
 print("\n")
+
+
+res = set()
+print(res)
+for x in range(10):
+    res.add(x * x)
+
+print(res)
+print(x)
+print("\n")
+
+res = {}
+for x in range(10):
+    res[x] = x * x
+
+print(res)
+print(x)
+print("\n")
+
+
+G = ((x, x * x) for x in range(10))
+print(next(G))
+print(next(G))
+print("\n")
+
+
+# set and dict's extended comprehension gramar
+
+print([x * x for x in range(10) if x % 2 == 0])
+print({x * x for x in range(10) if x % 2 == 0})
+print({x: x * x for x in range(10) if x % 2 == 0})
+print("\n")
+
+
+print([x + y for x in [1, 2, 3] for y in [4, 5, 6]])
+print({x + y for x in [1, 2, 3] for y in [4, 5, 6]})
+print({x: y for x in [1, 2, 3] for y in [4, 5, 6]})
+print("\n")
+
+
+print({x + y for x in "ab" for y in "cd"})
+
+print({x + y: (ord(x), ord(y)) for x in "ab" for y in "cd"})
+
+print({k * 2 for k in ["spam", "ham", "sausage"] if k[0] == "s"})
+
+print({k.upper(): k * 2 for k in ["spam", "ham", "sausage"] if k[0] == "s"})
