51752a81609d8b2b9186a02adbfcac5aaa5fe324
冉申 <ranshen0519@icloud.com>
2022-03-14 08:50:12 +0800

    Update simulate zip&map.py

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/simulate zip&map.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/simulate zip&map.py
@@ -124,3 +124,30 @@ def mymapPad(*seqs, pad=None):
 
 print(mymapPad(S1, S2))
 print(mymapPad(S1, S2, pad=99))
+print("\n")
+
+
+def myzip(*seqs):
+    minlen = min(len(S) for S in seqs)
+    return (tuple(S[i] for S in seqs) for i in range(minlen))
+
+
+S1, S2 = "abc", "xyz123"
+print(list(myzip(S1, S2)))
+print("\n")
+
+
+# notice: 单遍iteration
+
+
+def myzip(*args):
+    iters = map(iter, args)
+    while iters:
+        res = [next(i) for i in iters]
+        yield tuple(res)
+
+
+# # print(list(myzip("abc", "lmnop")))
+# def myzip(*args):
+#     iters = list(map(iter, args))
+#     ...rest as is...
