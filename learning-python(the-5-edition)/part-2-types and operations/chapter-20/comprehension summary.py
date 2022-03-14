830e4ad4214e12d6b74cce9c5bb0702dea3b259d
冉申 <ranshen0519@icloud.com>
2022-03-14 09:08:37 +0800

    Update comprehension summary.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension summary.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-20/comprehension summary.py
@@ -38,3 +38,27 @@ func()
 print("\n")
 
 print(x for x in range(5))
+print("\n")
+
+x = 99
+print([x for x in range(5)])
+
+print(x)
+print("\n")
+
+y = 99
+for y in range(5):
+    pass
+print(y)
+print("\n")
+
+
+# understand set-comprehension and dict-comprehension
+
+print({x * x for x in range(10)})
+print(set(x * x for x in range(10)))
+print("\n")
+
+print({x: x * x for x in range(10)})
+print(dict((x, x * x) for x in range(10)))
+print("\n")
