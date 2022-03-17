8907888b40ce1d6b6df722b13087278cdb3ec603
冉申 <ranshen0519@icloud.com>
2022-03-17 13:25:34 +0800

    Update part4 exercise.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-21/part4 exercise.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-21/part4 exercise.py
@@ -52,3 +52,48 @@ for func in (adder1, adder2):
     print(func("spam", "eggs", "toast"))
     print(func(["a", "b"], ["c", "d"], ["e", "f"]))
 print("\n")
+
+
+# 4 关键字parameter
+
+
+def adder(good=1, bad=2, ugly=3):
+    return good + bad + ugly
+
+
+print(adder())
+print(adder(5))
+print(adder(5, 6))
+print(adder(5, 6, 7))
+print(adder(ugly=7, good=6, bad=5))
+print("\n")
+
+
+# Second part solutions
+
+
+def adder1(*args):
+    tot = args[0]
+    for arg in args[1:]:
+        tot += arg
+    return tot
+
+
+def adder2(**args):
+    argskeys = list(args.keys())
+    tot = args[argskeys[0]]
+    for key in argskeys[1:]:
+        tot += args[key]
+    return tot
+
+
+def adder3(**args):
+    args = list(args.values())
+    tot = args[0]
+    for arg in args[1:]:
+        tot += arg
+    return tot
+
+
+def adder4(**args):
+    return adder1(*args.values())
