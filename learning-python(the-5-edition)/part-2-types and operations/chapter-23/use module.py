1d2e88fb78099af7232397774302a0b6605ff35b
冉申 <ranshen0519@icloud.com>
2022-03-18 16:20:20 +0800

    Update use module.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-23/use module.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-23/use module.py
@@ -35,3 +35,65 @@ print("\n")
 
 
 # 在模块中改变可变对象
+
+from small import x, y
+
+x = 42
+y[0] = 42
+print(x)
+print(y)
+print("\n")
+
+
+import small
+
+print(small.x)
+print(small.y)
+print("\n")
+
+
+# 跨文件的名称修改
+
+from small import x, y
+
+x = 42
+print(x)
+
+import small
+
+small.x = 42
+print(small.x)
+print("\n")
+
+
+# import 和 from 的等价性
+
+# from module import name1, name2
+# import module
+# name1 = module.name1
+# name2 = module.name2
+# del module
+
+# 文件产生命名空间
+
+import module2
+
+print(module2.sys)
+
+print(module2.name)
+
+print(module2.func)
+
+print(module2.klass)
+print("\n")
+
+# \命名空间字典： __dict__
+
+print(list(module2.__dict__.keys()))
+print("\n")
+
+print(list(name for name in module2.__dict__.keys() if not name.startswith("__")))
+print(list(name for name in module2.__dict__ if not name.startswith("__")))
+print("\n")
+
+print(module2.name, module2.__dict__["name"])
