c6e559c5eb85563f5bf030cc43491d26d8300146
冉申 <ranshen0519@icloud.com>
2022-03-18 08:43:10 +0800

    Update timesqrt.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-21/timesqrt.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-21/timesqrt.py
@@ -30,3 +30,37 @@ print(sys.version)
 for test in (mathMod, powCall, powExpr):
     elapsed, result = timer2.bestoftotal(test, _reps1=3, _reps=1000)
     print("%s: %.5f => %s" % (test.__name__, elapsed, result))
+print("\n")
+
+
+def dictcomp(I):
+    return {i: i for i in range(I)}
+
+
+def dictloop(I):
+    new = {}
+    for i in range(I):
+        new[i] = i
+    return new
+
+
+print(dictcomp(10))
+print(dictloop(10))
+print("\n")
+
+from timer2 import total, bestof
+
+print(bestof(dictcomp, 10000)[0])
+print(bestof(dictloop, 10000)[0])
+print("\n")
+
+print(bestof(dictcomp, 100000)[0])
+print(bestof(dictloop, 100000)[0])
+print("\n")
+
+print(bestof(dictcomp, 1000000)[0])
+print(bestof(dictloop, 1000000)[0])
+print("\n")
+
+print(bestof(dictcomp, 1000000, _reps=50)[0])
+print(bestof(dictloop, 1000000, _reps=50)[0])
