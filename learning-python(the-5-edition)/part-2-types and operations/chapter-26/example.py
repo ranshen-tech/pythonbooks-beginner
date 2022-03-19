e55400523fb651f361c2dab4048aaf432b22d7bc
冉申 <ranshen0519@icloud.com>
2022-03-19 16:09:34 +0800

    Create example.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-26/example.py
@@ -0,0 +1,34 @@
+class FirstClass:
+    def setdata(self, value):
+        self.data = value
+
+    def display(self):
+        print(self.data)
+
+
+x = FirstClass()
+y = FirstClass()
+
+x.setdata("King Arthur")
+y.setdata(3.14159)
+
+
+x.display()
+y.display()
+
+x.data = "New value"
+x.display()
+
+x.anothername = "spam"
+print("\n")
+
+
+class SecondClass(FirstClass):
+    def display(self):
+        print("Current value = %s" % self.data)
+
+
+z = SecondClass()
+z.setdata(42)
+z.display()
+print("\n")
