f8bd4db29686262de846ecb46d22a66a869084b7
冉申 <ranshen0519@icloud.com>
2022-03-19 11:15:10 +0800

    Create part5 exercises.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-25/part5 exercises.py
@@ -0,0 +1,29 @@
+# exercise 1
+
+import mymod
+
+print(mymod.test("chapter-25/mymod.py"))
+print("\n")
+
+
+import mymod1
+
+print(mymod1.test("chapter-25/mymod1.py"))
+print("\n")
+
+
+import mymod2
+
+print(mymod2.test("chapter-25/mymod2.py"))
+print("\n")
+
+
+# exercise 2
+
+from mymod import *
+
+print(countChars("chapter-25/mymod.py"))
+print("\n")
+
+
+# exercise 4
