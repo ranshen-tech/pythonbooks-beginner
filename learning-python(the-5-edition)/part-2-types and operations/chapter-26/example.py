e47d2444a0fbfff8b531f012fab1ec16c6d0d274
冉申 <ranshen0519@icloud.com>
2022-03-19 16:51:07 +0800

    Update example.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-26/example.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-26/example.py
@@ -31,4 +31,36 @@ class SecondClass(FirstClass):
 z = SecondClass()
 z.setdata(42)
 z.display()
+x.display()
+print("\n")
+
+
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+
+    def __str__(self):
+        return "[ThirdClass: %s]" % self.data
+
+    def mul(self, other):
+        self.data *= other
+
+
+a = ThirdClass("abc")
+print(a.data)
+a.display()
+print(a)
+print("\n")
+
+b = a + "xyz"
+print(b.data)
+b.display()
+print(b)
+print("\n")
+
+a.mul(3)
+print(a)
 print("\n")
