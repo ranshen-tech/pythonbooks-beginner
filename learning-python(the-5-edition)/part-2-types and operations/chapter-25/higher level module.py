02fdf51ef96e6b7469f0a6d6f0e2442d89b08925
冉申 <ranshen0519@icloud.com>
2022-03-19 09:54:43 +0800

    Create higher level module.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-25/higher level module.py
@@ -0,0 +1,26 @@
+from unders import *
+
+print(a, c)
+# print(_b)
+print("\n")
+
+
+# 混合使用模式：__name__ 和 __main__
+
+import runme
+
+runme.tester()
+print("\n")
+
+
+import minmax2
+
+print(minmax2.minmax(minmax2.lessthan, "s", "p", "a", "a"))
+print("\n")
+
+
+# import statement and from statement 的as扩展
+# import modulename as name
+
+
+# Module trap
