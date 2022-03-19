f31f01350a515f2471e1234b851c09d0de969db2
冉申 <ranshen0519@icloud.com>
2022-03-20 07:58:33 +0800

    Update example.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-26/example.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-26/example.py
@@ -64,3 +64,49 @@ print("\n")
 a.mul(3)
 print(a)
 print("\n")
+
+
+# simple class
+
+
+class rec:
+    pass
+
+
+rec.name = "Bob"
+rec.age = 40
+
+print(rec.name)
+
+x = rec()
+y = rec()
+print(x.name, y.name)
+
+x.name = "Sue"
+print(rec.name, x.name, y.name)
+print("\n")
+
+
+print(list(rec.__dict__.keys()))
+print(list(name for name in rec.__dict__ if not name.startswith("__")))
+print(list(x.__dict__.keys()))
+print(y.__dict__.keys())
+print("\n")
+
+
+print(x.name, x.__dict__["name"])
+print(x.age)
+# print(x.__dict__["age"])
+print("\n")
+
+print(x.__class__)
+# print(x.__bases__)
+print("\n")
+
+
+def uppername(obj):
+    return obj.name.upper()
+
+
+print(uppername(x))
+print("\n")
