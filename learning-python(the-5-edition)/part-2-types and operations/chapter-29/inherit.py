d56a4f2753b144ea5609a2584fd18931b0916d50
冉申 <ranshen0519@icloud.com>
2022-03-23 07:37:28 +0800

    Update inherit.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-29/inherit.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-29/inherit.py
@@ -17,3 +17,45 @@ print("\n")
 x = Sub()
 x.method()
 print("\n")
+
+
+# abstract super class
+class Super:
+    def delegate(self):
+        self.action()
+
+    def action(self):
+        assert False, "action must be defined!"
+
+
+X = Super()
+# X.delegate()
+
+
+class Super:
+    def delegate(self):
+        self.action()
+
+    def action(self):
+        raise NotImplementedError("action must be defined!")
+
+
+X = Super()
+# X.delegate()
+
+
+class Sub(Super):
+    pass
+
+
+X = Sub()
+# X.delegate()
+
+
+class Sub(Super):
+    def action(self):
+        print("spam")
+
+
+X = Sub()
+X.delegate()
