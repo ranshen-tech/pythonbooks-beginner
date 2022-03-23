2c4791cd98dc711c3b5f94312253869378a4bbc8
冉申 <ranshen0519@icloud.com>
2022-03-23 10:57:20 +0800

    Update namespace.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- learning-python(the-5-edition)/part-2-types and operations/chapter-29/namespace.py
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-29/namespace.py
@@ -31,3 +31,29 @@ print(list(Sub.__dict__.keys()))
 print(list(Super.__dict__.keys()))
 print(Y.__dict__)
 print("\n")
+
+print(X.data1, X.__dict__["data1"])
+X.data3 = "toast"
+print(X.__dict__)
+X.__dict__["data3"] = "ham"
+print(X.data3)
+print("\n")
+
+
+# docstring
+import docstr
+
+print(docstr.__doc__)
+print(docstr.func.__doc__)
+print("\n")
+
+print(docstr.spam.__doc__)
+print(docstr.spam.method.__doc__)
+print("\n")
+
+x = docstr.spam()
+x.method()
+print("\n")
+
+
+help(docstr)
