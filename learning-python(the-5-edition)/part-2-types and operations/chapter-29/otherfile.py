851f609c5252210c4617d79a5644b942e49491f1
冉申 <ranshen0519@icloud.com>
2022-03-23 08:24:08 +0800

    Create otherfile.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-29/otherfile.py
@@ -0,0 +1,45 @@
+import manynames
+
+X = 66
+print(X)
+print(manynames.X)
+print("\n")
+
+manynames.f()
+manynames.g()
+print("\n")
+
+print(manynames.C.X)
+I = manynames.C()
+print(I.X)
+print("\n")
+
+I.m()
+print(I.X)
+print("\n")
+
+X = 11
+
+
+def g1():
+    print(X)
+
+
+def g2():
+    global X
+    X = 22
+
+
+def h1():
+    X = 33
+
+    def nested():
+        print(X)
+
+
+def h2():
+    X = 33
+
+    def nested():
+        nonlocal X
+        X = 44
