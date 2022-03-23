db3620f1045a66696fb0f6e87581dc514aa779f1
冉申 <ranshen0519@icloud.com>
2022-03-23 09:09:51 +0800

    Create classcope.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ learning-python(the-5-edition)/part-2-types and operations/chapter-29/classcope.py
@@ -0,0 +1,51 @@
+X = 1
+
+
+def nester():
+    print(X)
+
+    class C:
+        print(X)
+
+        def method1(self):
+            print(X)
+
+        def method2(self):
+            X = 3
+            print(X)
+
+    I = C()
+    I.method1()
+    I.method2()
+
+
+print(X)
+nester()
+print("-" * 40)
+
+
+X = 1
+
+
+def nester():
+    X = 2
+    print(X)
+
+    class C:
+        print(X)
+
+        def method1(self):
+            print(X)
+
+        def method2(self):
+            X = 3
+            print(X)
+
+    I = C()
+    I.method1()
+    I.method2()
+
+
+print(X)
+nester()
+print("\n")
