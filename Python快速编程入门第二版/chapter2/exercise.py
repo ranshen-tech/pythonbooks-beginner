91bfbdfc61a88bbab4dd44f29253dda8f7df9c3a
冉申 <ranshen0519@icloud.com>
2022-03-29 09:55:57 +0800

    Update exercise.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- Python快速编程入门第二版/chapter2/exercise.py
+++ Python快速编程入门第二版/chapter2/exercise.py
@@ -6,3 +6,24 @@ print(bool(""))
 print(bool([]))
 print(bool(()))
 print(bool({}))
+print("\n")
+
+
+for item in range(10):
+    if item == 3:
+        break
+    print(item)
+else:
+    print("for over!")
+print("\n")
+
+
+i, r, s, t = 100, 0, 0, 0
+while i < 1000:
+    r = i // 100
+    s = (i - r * 100) // 10
+    t = i - r * 100 - s * 10
+    if i == r**3 + s**3 + t**3:
+        print("i =", str(i))
+    i += 1
+print("\n")
