a7bc8903ce0a6ef191257b3f2d3367e4a6f9f205
冉申 <ranshen0519@icloud.com>
2022-04-05 12:01:32 +0800

    Update demo.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- 看漫画学python/demo.py
+++ 看漫画学python/demo.py
@@ -120,3 +120,45 @@ print("\n")
 
 s = "Python"
 print("{}".format(s))
+print("\n")
+
+
+# 9.4.1 实例变量
+class Dog:
+    def __init__(self, name, age):
+        self.name = name  # 创建和初始化实例变量name
+        self.age = age  # 创建和初始化实例变量age
+
+
+d = Dog("球球", 2)
+print(f"我们家狗狗名叫{d.name},{d.age}岁了。")
+print("\n")
+
+
+# 9.4.2 构造方法
+class Dog:
+    def __init__(self, name, age, sex="雌性"):
+        self.name = name  # 创建和初始化实例变量name
+        self.age = age  # 创建和初始化实例变量age
+        self.sex = sex  # 创建和初始化实例变量sex
+
+
+d1 = Dog("球球", 2)
+d2 = Dog("哈哈", 1, "雄性")
+d3 = Dog(name="拖布", sex="雄性", age=3)
+
+print(f"{d1.name}: {d1.age}岁{d1.sex}")
+print(f"{d2.name}: {d2.age}岁{d2.sex}")
+print(f"{d3.name}: {d3.age}岁{d3.sex}")
+print("\n")
+
+
+# class Dog:
+#     # 构造方法
+#     def __init__(self, name, age, sex='雌性'):
+#         self.name = name # 创建和初始化实例变量name
+#         self.__age = age # 创建和初始化私有化变量__age
+
+#     # 实例方法
+#     def run(self):
+#         print(f'{self.name}在跑...')
