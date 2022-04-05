ce1e78b82353667c0ed82d0ae1e6f5f58008cceb
冉申 <ranshen0519@icloud.com>
2022-04-05 12:18:42 +0800

    Create class.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ 看漫画学python/class.py
@@ -0,0 +1,85 @@
+# 9.4.1 实例变量
+class Dog:
+    def __init__(self, name, age):
+        self.name = name  # 创建和初始化实例变量name
+        self.age = age  # 创建和初始化实例变量age
+
+
+d = Dog("球球", 2)
+print(f"我们家狗狗名叫{d.name},{d.age}岁了。")
+print("\n")
+
+
+# 9.4.2 构造方法
+class Dog:
+    def __init__(self, name, age, sex="雌性"):
+        self.name = name  # 创建和初始化实例变量name
+        self.age = age  # 创建和初始化实例变量age
+        self.sex = sex  # 创建和初始化实例变量sex
+
+
+d1 = Dog("球球", 2)
+d2 = Dog("哈哈", 1, "雄性")
+d3 = Dog(name="拖布", sex="雄性", age=3)
+
+print(f"{d1.name}: {d1.age}岁{d1.sex}")
+print(f"{d2.name}: {d2.age}岁{d2.sex}")
+print(f"{d3.name}: {d3.age}岁{d3.sex}")
+print("\n")
+
+
+# 9.4.3 实例方法
+class Dog:
+    # 构造方法
+    def __init__(self, name, age, sex="雌性"):
+        # 创建和初始化实例变量name
+        self.name = name
+        # 创建和初始化实例变量age
+        self.age = age
+        # 创建和初始化实例变量sex
+        self.sex = sex
+
+    # 实例方法
+    def run(self):
+        print(f"{self.name}在跑...")
+
+    # 实例方法
+    def speak(self, sound):
+        print(f'{self.name}在叫, "{sound}"!')
+
+
+dog = Dog("球球", 2)
+dog.run()
+dog.speak("汪汪汪")
+print("\n")
+
+
+# 9.4.4 类变量
+class Account:
+    # 类变量利率interest_rate
+    interest_rate = 0.0568
+    # 构造方法
+    def __init__(self, owner, amount):
+        # 创建并初始化实例变量owner
+        self.owner = owner
+        # 创建并初始化实例变量amount
+        self.amount = amount
+
+
+account = Account("Tony", 800000.0)
+
+print(f"账户名: {account.owner}")
+print(f"账户金额: {account.amount}")
+print(f"利率: {Account.interest_rate}")
+print("\n")
+
+
+# class Dog:
+#     # 构造方法
+#     def __init__(self, name, age, sex='雌性'):
+#         self.name = name # 创建和初始化实例变量name
+#         self.__age = age # 创建和初始化私有化变量__age
+
+#     # 实例方法
+#     def run(self):
+#         print(f'{self.name}在跑...')
