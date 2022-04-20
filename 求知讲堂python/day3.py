59d98b193e8fb341598e2aa3fdea05d687756598
冉申 <ranshen0519@icloud.com>
2022-04-20 15:44:02 +0800

    Update day3.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- 求知讲堂python/day3.py
+++ 求知讲堂python/day3.py
@@ -24,3 +24,32 @@ print(b)
 print(f"name的内存地址{id(name)}")
 print(f"b的内存地址{id(b)}")
 print("\n")
+
+
+dateStr = "i love Python"
+# find可以查找目标对象在序列对象中的位置
+print(dateStr.find("p"))
+print(dateStr.index("o"))
+print("\n")
+
+
+print(dateStr.startswith("i"))
+print(dateStr.startswith("io"))
+print(dateStr.endswith("o"))
+print(dateStr.endswith("n"))
+print(dateStr.endswith("on"))
+print("\n")
+
+
+print(dateStr.lower())
+print(dateStr.upper())
+print("\n")
+
+
+msg = "hello world"
+print(msg[0])
+print(msg[2:5])
+print(msg[2:])
+print(msg[:3])
+print(msg[::-1])
+print("\n")
