06792ddd18a0e3073fffb40401ca95295a605f21
冉申 <ranshen0519@icloud.com>
2022-04-21 11:40:53 +0800

    Update day4.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- 求知讲堂python/day4.py
+++ 求知讲堂python/day4.py
@@ -31,4 +31,45 @@ def printInfo(name, height, weight, hobby, pro):
 
 
 printInfo("冉总", 178, 62, "阅读", "计算机")
+print("---------------------------------")
+printInfo("ShawnRan", 180, 63, "投机", "计算机金融工程")
 print("\n")
+
+
+def sum(a, b):
+    """
+    自定义的加法函数
+    """
+    sum = a + b
+    print(sum)
+
+
+sum(20, 15)
+print("\n")
+
+
+def sum1(a=20, b=30):
+    """
+    默认参数
+    """
+    print(f"默认参数使用={a+b}")
+
+
+sum1()
+sum1(1)
+print("\n")
+
+
+def sum2(a, b=20):
+    """
+    默认参数始终在参数列表尾部
+    """
+    print(f"默认参数使用={a+b}")
+
+
+sum2(2, 4)
+sum2(5)
+print("\n")
+
+
+# 可变参数
