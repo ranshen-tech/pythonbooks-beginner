3b3ef421a367b7118bff4db76973f1b3031128d2
冉申 <ranshen0519@icloud.com>
2022-04-21 16:04:46 +0800

    Update day5.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- 求知讲堂python/day5.py
+++ 求知讲堂python/day5.py
@@ -37,3 +37,34 @@ def func(x):
 print(f"a的地址:{id(a)}")
 func(a)
 print("\n")
+
+
+l = []
+
+
+def test_renc(parms):
+    l.append(1)
+    print(f"{l}\n")
+    print(id(parms))
+    print(f"内部的l{parms}")
+
+
+# 在python中，万物皆对象，实参传递的就是对象的引用，这句话非常重要‼️
+print(id(l))
+test_renc(l)
+print(f"外部的变量对象{l}")
+print("\n")
+
+
+def computer(x, y):
+    """计算参数的和
+
+    Args:
+        x (_type_): int
+        y (_type_): int
+    """
+    return x + y
+
+
+print(computer(2, 3))
+print("\n")
