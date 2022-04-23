4a84f9b37eda451bb55f7c53b838cf5cea1af01b
冉申 <ranshen0519@icloud.com>
2022-04-23 16:19:17 +0800

    Create day6.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

---
+++ 求知讲堂python/day6.py
@@ -0,0 +1,18 @@
+print(abs(-32))
+print("\n")
+
+
+# round()对浮点数取近似值
+print(round(2.6))
+print(round(2.5))
+print(round(2.4))
+print(round(-2.6))
+print(round(-2.4))
+print(round(2.66, 1))
+print("\n")
+
+
+# pow()求幂运算
+print(pow(3, 3))
+print(3**3)
+print("\n")
