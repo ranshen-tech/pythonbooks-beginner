4dd3bd4095b0786ebe870c45033a31d40114dcfe
冉申 <ranshen0519@icloud.com>
2022-04-23 16:37:17 +0800

    Update day6.py
    Co-Authored-By: Ranshen <gongchangsihang@qq.com>

--- 求知讲堂python/day6.py
+++ 求知讲堂python/day6.py
@@ -16,3 +16,36 @@ print("\n")
 print(pow(3, 3))
 print(3**3)
 print("\n")
+
+
+# max()求最大值
+print(max(1, 3))
+print(max([1, 2, 3], [3, 2, 1], [4, 0, 0]))
+print(max((1, 3, 4), (5,)))
+print("\n")
+
+
+# sum()求和
+print(sum(range(50)))
+print(sum(range(50), 3))
+print("\n")
+
+
+# eval()动态执行字符串表达式
+a, b, c = 1, 2, 3
+print(eval("a + b + c"))
+print(eval("a + b, c"))
+print(type(eval("a+b+c")))
+print(eval("a* b + c -30"))
+print("\n")
+
+
+def test_func():
+    print("我执行了吗？")
+
+
+test_func()
+eval("test_func()")
+print(eval("test_func"))
+print(eval("a+b"))
+print("\n")
