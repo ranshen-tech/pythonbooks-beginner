45e9acd2b1dff2cbcda4994b8b559dc5bb206940
冉申 <ranshen0519@icloud.com>
2022-04-25 18:44:30 +0800

    Update day8.py

--- 求知讲堂python/day8.py
+++ 求知讲堂python/day8.py
@@ -1,25 +1,72 @@
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#         print("__init__方法被调用")
+#         # 析构方法，当对象被销毁，python解释器会自动调用
+
+#     def __del__(self):
+#         print("__del__对象被调用")
+#         print(f"{self.name}对象被销毁")
+
+
+# dog = Animal("旺财")
+# print("\n")
+
+
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#         print("这是构造初始化方法")
+
+#     def __del__(self):
+#         print("这是析构方法")
+
+
+# cat = Animal("才才")
+# print("\n")
+
+
 class Animal:
-    def __init__(self, name):
-        self.name = name
-        print("__init__方法被调用")
-        # 析构方法，当对象被销毁，python解释器会自动调用
+    def eat(self):
+        print("eat")
 
-    def __del__(self):
-        print("__del__对象被调用")
-        print(f"{self.name}对象被销毁")
+    def drink(self):
+        print("drink")
 
 
-dog = Animal("旺财")
+class Cat(Animal):
+    def mmj(self):
+        print("小猫喵喵叫")
+
+
+class Dog(Animal):
+    def wwj(self):
+        print("小狗汪汪叫")
+
+
+d1 = Dog()
+d1.eat()
+d1.wwj()
+c1 = Cat()
+c1.eat()
+c1.mmj()
 print("\n")
 
 
-class Animal:
-    def __init__(self, name):
-        self.name = name
-        print("这是构造初始化方法")
-
-    def __del__(self):
-        print("这是析构方法")
+class Immortal:
+    def fly(self):
+        print("神仙会飞")
 
 
-cat = Animal("才才")
+class Monkey:
+    def eat(self):
+        print("猴子喜欢吃桃")
+
+
+class MonkeyKing(Immortal, Monkey):
+    pass
+
+
+sun = MonkeyKing()
+sun.fly()
+sun.eat()
